16f630992a6410c6e2cabc8dbcf8f0b6967b93c3
Graham Dyson <grahamdyson@hotmail.com>
2018-06-17 11:47:50 +0100

    added a test harness that combines the two existing harnesses

---
+++ harness.source.js
@@ -0,0 +1,52 @@
+const
+	createElement = require("react").createElement,
+	formatYaml = require("js-yaml").safeDump,
+	getTextWidth = require("string-pixel-width"),
+	parse = require("acorn").parse,
+	parseYaml = require("js-yaml").safeLoad,
+	render = require("react-dom").render,
+	walk = require("acorn/dist/walk");
+
+const
+	generate = require("./Generators/JavaScript/Files/generate"),
+	getSvgForYaml = require("./Renderer/getSvgElementForYaml");
+
+const
+	javascriptTextArea = document.getElementById("javascript"),
+	svgDiv = document.getElementById("svg"),
+	yamlTextArea = document.getElementById("yaml");
+
+javascriptTextArea.addEventListener("input", generateFromTextareaIntoDiv);
+yamlTextArea.addEventListener("input", renderFromTextareaIntoDiv);
+
+generateFromTextareaIntoDiv();
+renderFromTextareaIntoDiv();
+
+function generateFromTextareaIntoDiv() {
+	const yaml =
+		generate({
+			file: parse(javascriptTextArea.value, { ecmaVersion: 9 }),
+			walk: walk.ancestor,
+		});
+
+	yamlTextArea.value =
+		yaml
+		?
+		formatYaml(yaml)
+		.trim()
+		:
+		"";
+
+	renderFromTextareaIntoDiv();
+}
+
+function renderFromTextareaIntoDiv() {
+	render(
+		getSvgForYaml({
+			createElement,
+			getTextWidth,
+			yaml: parseYaml(yamlTextArea.value),
+		}),
+		svgDiv
+	);
+}
---
+++ webpack.config.js
@@ -0,0 +1,1 @@
+module.exports = require("./getHarnessWebpackConfig")(__dirname);
