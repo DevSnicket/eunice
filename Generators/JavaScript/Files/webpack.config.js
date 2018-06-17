9a3d07dfa72da19670e38c5637c1d250d95df980
Graham Dyson <Graham.Dyson@emishealth.com>
2018-06-17 09:24:32 +0100

    Shared the test harnesses configuration for webpack (both harnesses need to use babel).

--- Generators/JavaScript/Files/webpack.config.js
+++ Generators/JavaScript/Files/webpack.config.js
@@ -1,18 +1,1 @@
-const path = require("path");
-
-module.exports = {
-	devtool: "source-map",
-	entry: "./harness.source.js",
-	module: {
-		rules: [
-			{
-				exclude: /node_modules/,
-				test: /\.js$/,
-			},
-		],
-	},
-	output: {
-		filename: "harness.bundle.js",
-		path: path.resolve(__dirname),
-	},
-};
+module.exports = require("../../../getHarnessWebpackConfig")(__dirname);
---
+++ getHarnessWebpackConfig.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports =
+	directory => (
+		{
+			devtool: "source-map",
+			entry: "./harness.source.js",
+			module: {
+				rules: [
+					{
+						exclude: /node_modules/,
+						test: /\.js$/,
+						use: {
+							loader: "babel-loader",
+							options: {
+								plugins: [ "transform-object-rest-spread" ],
+								presets: [ "env" ],
+							},
+						},
+					},
+				],
+			},
+			output: {
+				filename: "harness.bundle.js",
+				path: path.resolve(directory),
+			},
+		}
+	);
