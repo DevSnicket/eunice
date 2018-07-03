33caf8a90af71b2f3bea3f2f0f506ba95834acce
Graham Dyson <grahamdyson@hotmail.com>
2018-07-03 12:38:07 +0100

    createElementsContainer is now in a seperate file to getSvgElementForYaml creation of the root stack from parsed YAML and initalisation of dependencies in the stack is still in getSvgElementForYaml

--- dependency-counter/getSvgElementForYaml.js
+++ dependency-counter/getSvgElementForYaml.js
@@ -1,17 +1,8 @@
 const
-	countDependenciesOfItemRecursive = require("./getSvgForYaml/countDependenciesOfItemRecursive"),
 	createArrows = require("./getSvgForYaml/createArrows"),
-	createDependenciesInlineElements = require("./getSvgForYaml/createDependenciesInlineElements"),
-	createDependenciesInlineGroupFactories = require("./getSvgForYaml/createDependenciesInlineGroupFactories"),
-	createDependencyGroupFactoryWhenRequired = require("./getSvgForYaml/createDependencyGroupFactoryWhenRequired"),
-	createItemAndDependencyGroupsContainer = require("./getSvgForYaml/createItemAndDependencyGroupsContainer"),
-	createItemDependencyGroupsAndCalculateSize = require("./getSvgForYaml/createItemDependencyGroupsAndCalculateSize"),
-	createItemGroupFactory = require("./getSvgForYaml/createItemGroupFactory"),
-	createStackElementsContainer = require("./getSvgForYaml/createStackElementsContainer"),
+	createElementsContainer = require("./getSvgForYaml/createElementsContainer"),
 	createStackFromParsedYaml = require("./getSvgForYaml/createStackFromParsedYaml"),
-	createSummaryElementsContainer = require("./getSvgForYaml/createSummaryElementsContainer"),
 	createSvgElement = require("./getSvgForYaml/createSvgElement"),
-	getDependencyCountInBothDirections = require("./getSvgForYaml/getDependencyCountInBothDirections"),
 	initializeDependenciesInStack = require("./getSvgForYaml/initializeDependenciesInStack"),
 	withPrecision = require("./getSvgForYaml/withPrecision");
 
@@ -36,12 +27,7 @@ module.exports =
 				childElementsContainer:
 					yaml
 					&&
-					initaliseAndCreateElementsContainer({
-						arrows,
-						createElement,
-						font,
-						yaml,
-					}),
+					initaliseAndCreateElementsContainer(),
 				createElement,
 				font,
 				symbols:
@@ -49,6 +35,22 @@ module.exports =
 					.map(arrow => arrow.element),
 			})
 		);
+
+		function initaliseAndCreateElementsContainer() {
+			const stack = createStackFromParsedYaml(yaml);
+
+			initializeDependenciesInStack(stack);
+
+			return (
+				createElementsContainer({
+					arrows,
+					createElement,
+					font,
+					stack,
+					withPrecision,
+				})
+			);
+		}
 	};
 
 function createFont({
@@ -64,198 +66,5 @@ function createFont({
 			measure: text => withPrecision(getTextWidth(text, getTextWidthOptions)),
 			size,
 		}
-	);
-}
-
-function initaliseAndCreateElementsContainer({
-	arrows,
-	createElement,
-	font,
-	yaml,
-}) {
-	const stack = createStackFromParsedYaml(yaml);
-
-	initializeDependenciesInStack(stack);
-
-	const dependencyCounts = [];
-
-	return (
-		createSummaryElementsContainer({
-			arrows,
-			createInlineDependencyElements:
-				({
-					center,
-					count,
-					top,
-				}) =>
-					createDependenciesInlineElements({
-						center,
-						groupFactories:
-							createDependenciesInlineGroupFactoriesForCount(
-								count
-							),
-						top,
-					}),
-			dependencyCounts,
-			stackElementsContainer:
-				createStackElementsContainer({
-					addPadding,
-					createItemGroupsContainer:
-						({
-							items,
-							top,
-						}) =>
-							createItemAndDependencyGroupsContainer({
-								addPadding,
-								createItemAndDependencyGroup:
-									({
-										item,
-										left,
-									}) =>
-										countDependenciesOfAndCreateGroupsForItem({
-											item,
-											left,
-											top,
-										}),
-								items,
-								top,
-								withPrecision,
-							}),
-					stack,
-				}),
-		})
-	);
-
-	function countDependenciesOfAndCreateGroupsForItem({
-		item,
-		left,
-		top,
-	}) {
-		const dependencyCount = countDependenciesOfItemRecursive(item);
-
-		if (dependencyCount)
-			dependencyCounts.push(dependencyCount);
-
-		return (
-			createItemDependencyGroupsAndCalculateSize({
-				createGroupFactoryWhenRequired:
-					({ arrow, count }) =>
-						createDependencyGroupFactoryWhenRequired({
-							arrow,
-							count,
-							createTextGroup,
-							font,
-						}),
-				dependencyCount:
-					getDependencyCountInBothDirections({
-						arrows,
-						dependencyCount,
-					}),
-				itemGroupFactory:
-					createItemGroupFactory({
-						createTextGroup,
-						dependencyGroupFactories:
-							dependencyCount
-							&&
-							dependencyCount.dependsUpon
-							&&
-							dependencyCount.dependsUpon.inner
-							&&
-							createDependenciesInlineGroupFactoriesForCount(dependencyCount.dependsUpon.inner),
-						font,
-						identifier: item.id,
-					}),
-				left,
-				top,
-				withPrecision,
-			})
-		);
-	}
-
-	function createDependenciesInlineGroupFactoriesForCount(
-		countWithDirection
-	) {
-		return (
-			createDependenciesInlineGroupFactories({
-				arrows,
-				countWithDirection,
-				createDependencyGroupFactoryWhenRequired:
-					({ arrow, count }) =>
-						createDependencyGroupFactoryWhenRequired({
-							arrow,
-							count,
-							createTextGroup,
-							font,
-						}),
-			})
-		);
-	}
-
-	// x and y are attribute names in SVG
-	/* eslint id-length: ["error", { "exceptions": ["x", "y"] }] */
-	function createTextGroup({
-		attributes,
-		className,
-		elementName,
-		elementsBelowText,
-		height,
-		left,
-		paddingBottom = 0,
-		paddingRight,
-		text,
-		top,
-		width,
-	}) {
-		return (
-			createElement(
-				"g",
-				className && { className },
-				[
-					createElement(
-						elementName,
-						{
-							...attributes,
-							height,
-							width,
-							...left > 0 && { x: left },
-							...top > 0 && { y: top },
-						}
-					),
-					createElement(
-						"text",
-						{
-							x: withPrecision(left + getTextLeftOffset()),
-							y: withPrecision(top + getTextTopOffset()),
-						},
-						text
-					),
-					...elementsBelowText || [],
-				]
-			)
-		);
-
-		function getTextLeftOffset() {
-			return (width - paddingRight) / 2;
-		}
-
-		function getTextTopOffset() {
-			return (
-				((height - paddingBottom) / 2)
-				+
-				(font.size * 0.375)
-			);
-		}
-	}
-}
-
-function addPadding(
-	offset
-) {
-	return (
-		offset == 0
-		?
-		0
-		:
-		withPrecision(offset + 15)
 	);
 }
