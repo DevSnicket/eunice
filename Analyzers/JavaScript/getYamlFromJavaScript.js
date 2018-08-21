46b084565cd89165cf630a4656040ca46c898984
Graham Dyson <grahamdyson@hotmail.com>
2018-08-21 10:45:02 +0100

    made JavaScript analyser support being called from node / CLI

--- Analyzers/JavaScript/getYamlFromJavaScript.js
+++ Analyzers/JavaScript/getYamlFromJavaScript.js
@@ -1,11 +1,23 @@
 const
+	callWhenProcessEntryPoint = require("../../callWhenProcessEntryPoint"),
 	getItemOrItemsFromJavaScript = require("./getItemOrItemsFromJavaScript"),
 	getYamlForItemOrItems = require("./getYamlForItemOrItems");
 
-module.exports =
-	javaScript =>
+callWhenProcessEntryPoint({
+	action: getYamlFromJavaScript,
+	standardInputParameter: "javaScript",
+});
+
+module.exports = getYamlFromJavaScript;
+
+function getYamlFromJavaScript(
+	javaScript
+) {
+	return (
 		getYamlForItemOrItems(
 			getItemOrItemsFromJavaScript(
 				javaScript
 			)
-		);
+		)
+	);
+}
