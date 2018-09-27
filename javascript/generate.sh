726f3989b360149f76f910fff429442d46c26e2b
Graham Dyson <grahamdyson@hotmail.com>
2018-09-27 12:06:41 +0100

    added new processors to dogfooding to handle index.js file/folder structure

--- javascript/generate.sh
+++ javascript/generate.sh
@@ -5,8 +5,11 @@ mkdir $outputDirectory
 
 node Analyzers/JavaScript/getOrCreateItemsInDirectory --directory=. --ignoreDirectoryNames=Coverage --ignoreDirectoryNames=testcases --ignoreDirectoryNames=node_modules --ignoreDirectoryNames=Output | 
 node Processors/setTypeOfRootItems --type=file |
+node Processors/removeIdentifierSuffix --suffix=/index |
+node Processors/orderItemsBy/identifier |
 node Processors/groupItemsByIdentifierSeparator --identifierSeparator=/ |
-node Processors/orderItemsByType --typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file |
+node Processors/orderItemsBy/indexOf/type --typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file |
+node Processors/orderItemsBy/indexOf/identifierSuffix --identifierSuffixesInOrder=/test |
 node Processors/unstackIndependent |
 node Processors/stackRootItems --levels=harness --levels=Analyzers/JavaScript,Processors,Renderer --levels=callWhenProcessEntryPoint,dependencyAndStructure,Harnesses,Tests > $outputDirectory/.yaml
 
