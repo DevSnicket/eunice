ef4130794db512313601b9381fa1a9863063f160
Graham Dyson <grahamdyson@hotmail.com>
2018-10-04 20:41:22 +0100

    added a processor to remove self dependent items of type e.g. variables after removing redundant parent identifier prefixes

--- dogfooding/generate.sh
+++ dogfooding/generate.sh
@@ -9,6 +9,7 @@ node Processors/removeIdentifierSuffix --suffix=/index |
 node Processors/orderItemsBy/identifier |
 node Processors/groupItemsByIdentifierSeparator --identifierSeparator=/ |
 node Processors/removeRedundantParentIdentifierPrefix --identifierSeparator=/ |
+node Processors/removeSelfDependentItemsOfType --type=variable |
 node Processors/orderItemsBy/indexOf/type --typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file |
 node Processors/orderItemsBy/indexOf/identifierSuffix --identifierSuffixesInOrder=/test |
 node Processors/unstackIndependent |
