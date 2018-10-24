3a3b30c2d1cc39f473bd57649c6321a0cd917771
Graham Dyson <grahamdyson@hotmail.com>
2018-10-24 10:49:34 +0100

    dogfooding bash script now outputs each processor stage this is to help make it easier to understand what each stage does and to debug them

--- javascript/generate.sh
+++ javascript/generate.sh
@@ -3,17 +3,65 @@ outputDirectory=$(dirname $0)/output
 
 mkdir $outputDirectory
 
-node Analyzer/getOrCreateItemsInDirectory --directory=. --ignoreDirectoryNames=Coverage --ignoreDirectoryNames=testcases --ignoreDirectoryNames=node_modules --ignoreDirectoryNames=Output | 
-node Processors/setTypeOfRootItems --type=file |
-node Processors/removeIdentifierSuffix --suffix=/index |
-node Processors/orderItemsBy/identifier |
-node Processors/groupItemsByIdentifierSeparator --identifierSeparator=/ |
-node Processors/removeRedundantParentIdentifierPrefix --identifierSeparator=/ |
-node Processors/removeSelfDependentItemsOfType --type=variable |
-node Processors/orderItemsBy/indexOf/type --typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file |
-node Processors/createOrAddToStacks/uniformly --commaSeparatedLevels=test --commaSeparatedLevels=existing |
-node Processors/createOrAddToStacks/toItemsWithIdentifier --commaSeparatedLevels=existing --commaSeparatedLevels=expect,test --toIdentifier=test |
-node Processors/createOrAddToStacks/usingFileSystem --directory=$(dirname $0)/.. |
-node Processors/unstackIndependent > $outputDirectory/.yaml
+node Analyzer/getOrCreateItemsInDirectory \
+  --directory=. \
+  --ignoreDirectoryNames=Coverage --ignoreDirectoryNames=testcases --ignoreDirectoryNames=node_modules --ignoreDirectoryNames=Output \
+> $outputDirectory/analysis.yaml
 
-cat $outputDirectory/.yaml | node Renderer/getSvgForYaml > $outputDirectory/.svg
+cat $outputDirectory/analysis.yaml \
+| node Processors/setTypeOfRootItems \
+  --type=file \
+> $outputDirectory/setTypeOfRootItemsToFile.yaml
+
+cat $outputDirectory/setTypeOfRootItemsToFile.yaml \
+| node Processors/removeIdentifierSuffix \
+  --suffix=/index \
+> $outputDirectory/removeIdentifierSuffixOfItem.yaml
+
+cat $outputDirectory/removeIdentifierSuffixOfItem.yaml \
+| node Processors/orderItemsBy/identifier \
+> $outputDirectory/orderItemsByIdentifier.yaml
+
+cat $outputDirectory/orderItemsByIdentifier.yaml \
+| node Processors/groupItemsByIdentifierSeparator \
+  --identifierSeparator=/ \
+> $outputDirectory/groupItemsByIdentifierSeparatorOfSlash.yaml
+
+cat $outputDirectory/groupItemsByIdentifierSeparatorOfSlash.yaml \
+| node Processors/removeRedundantParentIdentifierPrefix \
+  --identifierSeparator=/ \
+> $outputDirectory/removeRedundantParentIdentifierPrefixOfSlash.yaml
+
+cat $outputDirectory/removeRedundantParentIdentifierPrefixOfSlash.yaml \
+| node Processors/removeSelfDependentItemsOfType \
+  --type=variable \
+> $outputDirectory/removeSelfDependentItemsOfTypeVariable.yaml
+
+cat $outputDirectory/removeSelfDependentItemsOfTypeVariable.yaml \
+| node Processors/orderItemsBy/indexOf/type \
+  --typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file \
+> $outputDirectory/orderItemsByIndexOfType.yaml
+
+cat $outputDirectory/orderItemsByIndexOfType.yaml \
+| node Processors/createOrAddToStacks/uniformly \
+  --commaSeparatedLevels=test --commaSeparatedLevels=existing \
+> $outputDirectory/createOrAddToStacksStackUniformlyForTest.yaml
+
+cat $outputDirectory/createOrAddToStacksStackUniformlyForTest.yaml \
+| node Processors/createOrAddToStacks/toItemsWithIdentifier \
+  --commaSeparatedLevels=existing --commaSeparatedLevels=expect,test \
+  --toIdentifier=test \
+> $outputDirectory/createOrAddToStacksStackToItemsWithIdentifierOfTest.yaml
+
+cat $outputDirectory/createOrAddToStacksStackToItemsWithIdentifierOfTest.yaml \
+| node Processors/createOrAddToStacks/usingFileSystem \
+  --directory=$(dirname $0)/.. \
+> $outputDirectory/createOrAddToStacksStackUsingFileSystem.yaml
+
+cat $outputDirectory/createOrAddToStacksStackUsingFileSystem.yaml \
+| node Processors/unstackIndependent \
+> $outputDirectory/.yaml
+
+cat $outputDirectory/.yaml \
+| node Renderer/getSvgForYaml \
+> $outputDirectory/.svg
