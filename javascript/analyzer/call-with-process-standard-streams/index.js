f7ee6a2f25d67ff8ab130719efd2f379c9a26fa5
Graham Dyson <grahamdyson@hotmail.com>
2018-10-26 13:57:20 +0100

    removed all packages from root other than coveralls this is a step towards separate packages and repos inc. shared/supporting each sub-directory now has its own installation of packages replaced tests in Renderer with ones in dependencyAndStructure as thats where the implementation now is renamed analyzer testcases directory to test-cases as when dogfooding it can't ignore "testcases" as thats also used to store JavaScript in dependencyAndStructure, but the former is code thats run to create testcases (not actual test cases as it is for the analyzer)

---
+++ javascript/analyzer/call-with-process-standard-streams/index.js
@@ -0,0 +1,59 @@
+/* istanbul ignore file: only used when JavaScript file is process entry point */
+
+const minimist = require("minimist");
+
+module.exports =
+	({
+		action,
+		parentModule = module.parent,
+		standardInputParameter = null,
+	}) => {
+		return isMain() && callWithProcessArgumentsAndStandardStreams();
+
+		function isMain() {
+			return require.main === parentModule;
+		}
+
+		function callWithProcessArgumentsAndStandardStreams() {
+			const processArguments = minimist(process.argv.slice(2));
+
+			if (standardInputParameter && !processArguments[standardInputParameter])
+				callWithProcessArgumentsAndStandardInputStream();
+			else
+				call(processArguments);
+
+			function callWithProcessArgumentsAndStandardInputStream() {
+				const standardIn = process.stdin;
+
+				standardIn.resume();
+				standardIn.setEncoding("utf8");
+
+				let input = "";
+
+				standardIn.on(
+					"data",
+					chunk => input += chunk,
+				);
+
+				standardIn.on(
+					"end",
+					() =>
+						call({
+							...processArguments,
+							[standardInputParameter]: input,
+						}),
+				);
+			}
+
+			function call(
+				processArgumentsAndOrStandardInput,
+			) {
+				// eslint-disable-next-line no-console
+				console.log(
+					action(
+						processArgumentsAndOrStandardInput,
+					),
+				);
+			}
+		}
+	};
