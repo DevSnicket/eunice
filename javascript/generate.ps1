12bb8a319465424b9732c75ea7bbff7118bc9d11
Graham Dyson <grahamdyson@hotmail.com>
2018-10-29 17:56:23 +0000

    replaced dogfooding on Windows batch file with a PowerShell script regular expressions were being used in the Bash version of script and unlike batch files, PowerShell has this functionality built-in the PowerShell script is also alot more similar to the Bash script than the batch file was.

---
+++ javascript/generate.ps1
@@ -0,0 +1,98 @@
+$rootDirectory = "$PSScriptRoot\.."
+$outputDirectory = "$PSScriptRoot\output"
+
+if (!(test-path $outputDirectory))
+{
+      New-Item -ItemType Directory -Force -Path $outputDirectory
+}
+
+# analyze this repository
+
+node $rootDirectory/javascript-analyzer/getOrCreateItemsInDirectory `
+  --directory=$rootDirectory `
+  --ignoreDirectoryNames=coverage --ignoreDirectoryNames=node_modules --ignoreDirectoryNames=output --ignoreDirectoryNames=test-cases --ignoreDirectoryNames=test-coverage `
+> $outputDirectory/analysis.yaml
+
+cat -Path $outputDirectory/analysis.yaml | `
+% {$_ -replace "'@devsnicket/eunice-run-tests-from-file-system'", "run-tests-from-file-system" } `
+> $outputDirectory/analysis-without-prefix.yaml
+
+# analyze run-tests-from-file-system package
+
+npm install --prefix $outputDirectory @devsnicket/eunice-run-tests-from-file-system
+
+node $rootDirectory/javascript-analyzer/getOrCreateItemsInDirectory `
+  --directory=$outputDirectory/node_modules/@devsnicket/eunice-run-tests-from-file-system `
+> $outputDirectory/run-tests-from-file-system.yaml
+
+cat $outputDirectory/run-tests-from-file-system.yaml `
+| % {$_ -replace "^- id: ", "- id: run-tests-from-file-system\" } `
+> $outputDirectory/run-tests-from-file-system-with-prefix.yaml
+
+# process and render this repository and packages
+
+cat `
+	$outputDirectory/analysis-without-prefix.yaml, `
+	$outputDirectory/run-tests-from-file-system-with-prefix.yaml `
+| node $rootDirectory/Processors/setTypeOfRootItems `
+	--type=file `
+> $outputDirectory/setTypeOfRootItemsToFile.yaml
+
+cat $outputDirectory/setTypeOfRootItemsToFile.yaml `
+| node $rootDirectory/Processors/removeIdentifierSuffix `
+  --suffix=\index `
+> $outputDirectory/removeIdentifierSuffixOfIndex.yaml
+
+cat $outputDirectory/removeIdentifierSuffixOfIndex.yaml `
+| node $rootDirectory/Processors/orderItemsBy/identifier `
+> $outputDirectory/orderItemsByIdentifier.yaml
+
+cat $outputDirectory/orderItemsByIdentifier.yaml `
+| node $rootDirectory/Processors/groupItemsByIdentifierSeparator `
+  --identifierSeparator=\ `
+> $outputDirectory/groupItemsByIdentifierSeparatorOfSlash.yaml
+
+cat $outputDirectory/groupItemsByIdentifierSeparatorOfSlash.yaml `
+| node $rootDirectory/Processors/removeRedundantParentIdentifierPrefix `
+  --identifierSeparator=\ `
+> $outputDirectory/removeRedundantParentIdentifierPrefixOfSlash.yaml
+
+cat $outputDirectory/removeRedundantParentIdentifierPrefixOfSlash.yaml `
+| node $rootDirectory/Processors/removeSelfDependentItemsOfType `
+  --type=variable `
+> $outputDirectory/removeSelfDependentItemsOfTypeVariable.yaml
+
+cat $outputDirectory/removeSelfDependentItemsOfTypeVariable.yaml `
+| node $rootDirectory/Processors/orderItemsBy/indexOf/type `
+  --typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file `
+> $outputDirectory/orderItemsByIndexOfType.yaml
+
+cat $outputDirectory/orderItemsByIndexOfType.yaml `
+| node $rootDirectory/Processors/createOrAddToStacks/uniformly `
+  --commaSeparatedLevels=test --commaSeparatedLevels=existing `
+> $outputDirectory/createOrAddToStacksStackUniformlyForTest.yaml
+
+cat $outputDirectory/createOrAddToStacksStackUniformlyForTest.yaml `
+| node $rootDirectory/Processors/createOrAddToStacks/toItemsWithIdentifier `
+  --commaSeparatedLevels=existing --commaSeparatedLevels=expect,test `
+  --toIdentifier=test `
+> $outputDirectory/createOrAddToStacksStackToItemsWithIdentifierOfTest.yaml
+
+cat $outputDirectory/createOrAddToStacksStackToItemsWithIdentifierOfTest.yaml `
+| node $rootDirectory/Processors/createOrAddToStacks/usingFileSystem `
+  --directory=$rootDirectory `
+> $outputDirectory/createOrAddToStacksStackUsingFileSystemInRepository.yaml
+
+cat $outputDirectory/createOrAddToStacksStackUsingFileSystemInRepository.yaml `
+| node $rootDirectory/Processors/createOrAddToStacks/usingFileSystem `
+   --directory=$outputDirectory/node_modules/@devsnicket/eunice-run-tests-from-file-system `
+   --subsetIdentifierHierarchy=run-tests-from-file-system `
+> $outputDirectory/createOrAddToStacksStackUsingFileSystemInRunTestsFromFileSystemPackage.yaml
+
+cat $outputDirectory/createOrAddToStacksStackUsingFileSystemInRunTestsFromFileSystemPackage.yaml `
+| node $rootDirectory/Processors/unstackIndependent `
+> $outputDirectory/.yaml
+
+cat $outputDirectory/.yaml `
+| node $rootDirectory/Renderer/getSvgForYaml `
+> $outputDirectory/.svg
