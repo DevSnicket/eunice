b1e0eab8b109e5872a594a152ed7bb4b9bf99399
Graham Dyson <grahamdyson@hotmail.com>
2018-10-31 09:09:41 +0000

    replaced "dependency and structure" code in repo with package dogfooding does not support hierarchical/path dependencies in the packages

--- stacking-explicit/removeRedundantParentIdentifierPrefix/index.js
+++ stacking-explicit/removeRedundantParentIdentifierPrefix/index.js
@@ -1,7 +1,6 @@
 const
 	callWithYamlItemsAndOutputWhenProcessEntryPoint = require("../callWithYamlItemsAndOutputWhenProcessEntryPoint"),
-	createStackFromYaml = require("../../dependencyAndStructure/createStackFromYaml"),
-	createYamlFromStack = require("../../dependencyAndStructure/createYamlFromStack"),
+	{ createStackFromYaml, createYamlFromStack } = require("@devsnicket/eunice-dependency-and-structure"),
 	processorPlugins = require("../../Harnesses/processorPlugins");
 
 callWithYamlItemsAndOutputWhenProcessEntryPoint(
