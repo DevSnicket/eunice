4092fd5f8c67ad7e5c851102d2c00bd9d6531a8c
Graham Dyson <grahamdyson@hotmail.com>
2018-11-04 16:28:40 +0000

    order items by identifier processor now puts anonymous items first

--- stacking-explicit/orderItemsBy/identifier/index.js
+++ stacking-explicit/orderItemsBy/identifier/index.js
@@ -47,6 +47,18 @@ function compareIdentifiers(
 	return (
 		left !== right
 		&&
-		(left < right ? -1 : 1)
+		(whenNoValues() || compareValues())
 	);
+
+	function whenNoValues() {
+		return (
+			(!left && -1)
+			||
+			(!right && 1)
+		);
+	}
+
+	function compareValues() {
+		return left < right ? -1 : 1;
+	}
 }
