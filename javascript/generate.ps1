51227c74cf423aa3935399af95acef557c589c96
Graham Dyson <grahamdyson@hotmail.com>
2018-11-06 09:32:27 +0000

    dogfooding supports hierarchical/path dependencies in the packages uses previous commits to add required functionality to processors

--- javascript/generate.ps1
+++ javascript/generate.ps1
@@ -1,117 +1,137 @@
 $ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['Out-File:Encoding'] = "utf8"
 
-$rootDirectory = "$PSScriptRoot\.."
-$outputDirectory = "$PSScriptRoot\output"
 
-if (!(test-path $outputDirectory))
-{
-      New-Item -ItemType Directory -Force -Path $outputDirectory
+$identifierSeparator="\"
+$rootDirectory = join-path $PSScriptRoot ".."
+$outputDirectory = join-path $PSScriptRoot "output"
+
+function ensureDirectoryExists() {
+	if (!(test-path $args[0]))
+	{
+		New-Item -ItemType Directory -Force -Path $args[0] | Out-Null
+	}
 }
 
-# analyze this repository
+ensureDirectoryExists outputDirectory
 
-node $rootDirectory/javascript-analyzer/getOrCreateItemsInDirectory `
-  --directory=$rootDirectory `
-  --ignoreDirectoryNames=coverage --ignoreDirectoryNames=node_modules --ignoreDirectoryNames=output --ignoreDirectoryNames=test-cases --ignoreDirectoryNames=test-coverage `
-> $outputDirectory/analysis-of-repository.yaml
+function processInDirectory {
+	$yamlDirectory = $args[0]
 
-# analyze run-tests-from-file-system package
+	cat $yamlDirectory/analysis.yaml `
+	| node $rootDirectory/Processors/setTypeOfRootItems `
+		--type=file `
+	> $yamlDirectory/set-type-of-root-items-to-file.yaml
+
+	cat $yamlDirectory/set-type-of-root-items-to-file.yaml `
+	| node $rootDirectory/Processors/replaceIdentifiers `
+		--pattern="\${identifierSeparator}index$|^index" `
+		--replacement= `
+	> $yamlDirectory/remove-identifier-suffix-of-index.yaml
+
+	cat $yamlDirectory/remove-identifier-suffix-of-index.yaml `
+	| node $rootDirectory/Processors/orderItemsBy/identifier `
+	> $yamlDirectory/order-items-by-identifier.yaml
+
+	cat $yamlDirectory/order-items-by-identifier.yaml `
+	| node $rootDirectory/Processors/groupItemsByIdentifierSeparator `
+		--identifierSeparator=$identifierSeparator `
+	> $yamlDirectory/group-items-by-identifier-separator-of-slash.yaml
+
+	cat $yamlDirectory/group-items-by-identifier-separator-of-slash.yaml `
+	| node $rootDirectory/Processors/removeRedundantParentIdentifierPrefix `
+		--identifierSeparator=$identifierSeparator `
+	> $yamlDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml
+
+	cat $yamlDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml `
+	| node $rootDirectory/Processors/removeSelfDependentItemsOfType `
+		--type=variable `
+	> $yamlDirectory/remove-self-dependent-items-of-type-variable.yaml
+
+	cat $yamlDirectory/remove-self-dependent-items-of-type-variable.yaml `
+	| node $rootDirectory/Processors/orderItemsBy/indexOf/type `
+		--typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file `
+	> $yamlDirectory/order-items-by-index-of-type.yaml
+
+	cat $yamlDirectory/order-items-by-index-of-type.yaml `
+	| node $rootDirectory/Processors/createOrAddToStacks/uniformly `
+		--commaSeparatedLevels=test --commaSeparatedLevels=existing `
+	> $yamlDirectory/stack-test-in-top-level.yaml
+
+	cat $yamlDirectory/stack-test-in-top-level.yaml `
+	| node $rootDirectory/Processors/createOrAddToStacks/toItemsWithIdentifier `
+		--commaSeparatedLevels=existing --commaSeparatedLevels=expect,test `
+		--toIdentifier=test `
+	> $yamlDirectory/add-to-stack-test-identifiers.yaml
+
+	cat $yamlDirectory/add-to-stack-test-identifiers.yaml `
+	| node $rootDirectory/Processors/unstackIndependent `
+	> $yamlDirectory/unstack-independent.yaml
+}
 
 $packages = "call-when-process-entry-point", "dependency-and-structure", "run-tests-from-file-system"
 
-cat -Path $outputDirectory/analysis-of-repository.yaml `
+# repository
+
+ensureDirectoryExists $outputDirectory/repository
+
+node $rootDirectory/javascript-analyzer/getOrCreateItemsInDirectory `
+	--directory=$rootDirectory `
+	--ignoreDirectoryNames=coverage --ignoreDirectoryNames=node_modules --ignoreDirectoryNames=output --ignoreDirectoryNames=test-cases --ignoreDirectoryNames=test-coverage `
+> $outputDirectory/repository/analysis.yaml
+
+processInDirectory $outputDirectory/repository
+
+cat -Path $outputDirectory/repository/unstack-independent.yaml `
 | % {$_ -replace "'@devsnicket/eunice-$($packages[0])'", $packages[0] } `
 | % {$_ -replace "'@devsnicket/eunice-$($packages[1])'", $packages[1] } `
 | % {$_ -replace "'@devsnicket/eunice-$($packages[2])'", $packages[2] } `
-> $outputDirectory/analysis-of-repository-without-package-prefixes.yaml
+> $outputDirectory/repository/without-package-prefixes.yaml
 
-foreach ($package in $packages) 
+# packages
+
+foreach ($package in $packages)
 {
 	npm install --no-save --prefix $outputDirectory @devsnicket/eunice-$package
+
+	ensureDirectoryExists $outputDirectory/$package
 
 	node $rootDirectory/javascript-analyzer/getOrCreateItemsInDirectory `
 		--directory=$outputDirectory/node_modules/@devsnicket/eunice-$package `
 		--ignoreDirectoryNames=dist `
 		--ignoreDirectoryNames=node_modules `
- 		> $outputDirectory/analysis-of-package-$package.yaml
+	> $outputDirectory/$package/analysis.yaml
 
-	cat $outputDirectory/analysis-of-package-$package.yaml `
-	| % {$_ -replace "^- id: ", "- id: $package\" } `
-	| % {$_ -replace "^- (?!id: )", "- $package\" } `
-	> $outputDirectory/analysis-of-package-$package-with-root-prefix.yaml
-}
+	processInDirectory $outputDirectory/$package
 
-# process and render this repository and packages
-
-cat `
-	$outputDirectory/analysis-of-repository-without-package-prefixes.yaml, `
-	$outputDirectory/analysis-of-package-$($packages[0])-with-root-prefix.yaml, `
-	$outputDirectory/analysis-of-package-$($packages[1])-with-root-prefix.yaml, `
-	$outputDirectory/analysis-of-package-$($packages[2])-with-root-prefix.yaml `
-| node $rootDirectory/Processors/setTypeOfRootItems `
-	--type=file `
-> $outputDirectory/set-type-of-root-items-to-file.yaml
-
-cat $outputDirectory/set-type-of-root-items-to-file.yaml `
-| node $rootDirectory/Processors/removeIdentifierSuffix `
-	--suffix=\index `
-> $outputDirectory/remove-identifier-suffix-of-index.yaml
-
-cat $outputDirectory/remove-identifier-suffix-of-index.yaml `
-| node $rootDirectory/Processors/orderItemsBy/identifier `
-> $outputDirectory/order-items-by-identifier.yaml
-
-cat $outputDirectory/order-items-by-identifier.yaml `
-| node $rootDirectory/Processors/groupItemsByIdentifierSeparator `
-	--identifierSeparator=\ `
-> $outputDirectory/group-items-by-identifier-separator-of-slash.yaml
-
-cat $outputDirectory/group-items-by-identifier-separator-of-slash.yaml `
-| node $rootDirectory/Processors/removeRedundantParentIdentifierPrefix `
-	--identifierSeparator=\ `
-> $outputDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml
-
-cat $outputDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml `
-| node $rootDirectory/Processors/removeSelfDependentItemsOfType `
-  --type=variable `
-> $outputDirectory/remove-self-dependent-items-of-type-variable.yaml
-
-cat $outputDirectory/remove-self-dependent-items-of-type-variable.yaml `
-| node $rootDirectory/Processors/orderItemsBy/indexOf/type `
-  --typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file `
-> $outputDirectory/order-items-by-index-of-type.yaml
-
-cat $outputDirectory/order-items-by-index-of-type.yaml `
-| node $rootDirectory/Processors/createOrAddToStacks/uniformly `
-  --commaSeparatedLevels=test --commaSeparatedLevels=existing `
-> $outputDirectory/stack-test-in-top-level.yaml
-
-cat $outputDirectory/stack-test-in-top-level.yaml `
-| node $rootDirectory/Processors/createOrAddToStacks/toItemsWithIdentifier `
-  --commaSeparatedLevels=existing --commaSeparatedLevels=expect,test `
-  --toIdentifier=test `
-> $outputDirectory/add-to-stack-test-identifiers.yaml
-
-cat $outputDirectory/add-to-stack-test-identifiers.yaml `
-| node $rootDirectory/Processors/createOrAddToStacks/usingFileSystem `
-  --directory=$rootDirectory `
-> $outputDirectory/stack-using-files-in-repository.yaml
-
-$lastStackUsingFiles = "repository"
-
-foreach ($package in $packages) 
-{
-	cat $outputDirectory/stack-using-files-in-$lastStackUsingFiles.yaml `
+	cat $outputDirectory/$package/unstack-independent.yaml `
 	| node $rootDirectory/Processors/createOrAddToStacks/usingFileSystem `
 		--directory=$outputDirectory/node_modules/@devsnicket/eunice-$package `
-		--subsetIdentifierHierarchy=$package `
-	> $outputDirectory/stack-using-files-in-$package.yaml
+		--subsetIdentifierHierarchy= `
+	> $outputDirectory/$package/stack-using-files.yaml
 
-	$lastStackUsingFiles=$package
+	cat $outputDirectory/$package/stack-using-files.yaml `
+	| node $rootDirectory/Processors/replaceIdentifiers `
+		--pattern=.+ `
+		--replacement="$package$identifierSeparator$&" `
+		--rootOnly=true `
+	| node $rootDirectory/Processors/replaceIdentifiers `
+		--pattern=^$ `
+		--replacement=$package `
+		--rootOnly=true `
+	> $outputDirectory/$package/with-root-prefix.yaml
 }
 
-cat $outputDirectory/stack-using-files-in-$lastStackUsingFiles.yaml `
-| node $rootDirectory/Processors/unstackIndependent `
+node $rootDirectory/Processors/concatenateFromFileSystem `
+	--files $outputDirectory/repository/without-package-prefixes.yaml `
+	--files $outputDirectory/$($packages[0])/with-root-prefix.yaml `
+	--files $outputDirectory/$($packages[1])/with-root-prefix.yaml `
+	--files $outputDirectory/$($packages[2])/with-root-prefix.yaml `
+> $outputDirectory/concatenate.yaml
+
+cat $outputDirectory/concatenate.yaml `
+| node $rootDirectory/Processors/createOrAddToStacks/usingFileSystem `
+	--directory=$rootDirectory `
 > $outputDirectory/.yaml
 
 cat $outputDirectory/.yaml `
--- javascript/generate.sh
+++ javascript/generate.sh
@@ -1,121 +1,135 @@
 #!/bin/bash
 set -e
 
+identifierSeparator=/
 rootDirectory=$(dirname $0)/..
 outputDirectory=$(dirname $0)/output
 
-if [ ! -d $outputDirectory ]; then
-	mkdir $outputDirectory
-fi
+function ensureDirectoryExists() {
+  if [ ! -d $1 ]; then
+    mkdir $1
+  fi
+}
 
-# analyze this repository
+function processInDirectory {
+  yamlDirectory=$1
+
+  cat $yamlDirectory/analysis.yaml \
+  | node $rootDirectory/Processors/setTypeOfRootItems \
+    --type=file \
+  > $yamlDirectory/set-type-of-root-items-to-file.yaml
+
+  cat $yamlDirectory/set-type-of-root-items-to-file.yaml \
+  | node $rootDirectory/Processors/replaceIdentifiers \
+    --pattern="${identifierSeparator}index$|^index" \
+    --replacement= \
+  > $yamlDirectory/remove-identifier-suffix-of-index.yaml
+
+  cat $yamlDirectory/remove-identifier-suffix-of-index.yaml \
+  | node $rootDirectory/Processors/orderItemsBy/identifier \
+  > $yamlDirectory/order-items-by-identifier.yaml
+
+  cat $yamlDirectory/order-items-by-identifier.yaml \
+  | node $rootDirectory/Processors/groupItemsByIdentifierSeparator \
+    --identifierSeparator=$identifierSeparator \
+  > $yamlDirectory/group-items-by-identifier-separator-of-slash.yaml
+
+  cat $yamlDirectory/group-items-by-identifier-separator-of-slash.yaml \
+  | node $rootDirectory/Processors/removeRedundantParentIdentifierPrefix \
+    --identifierSeparator=$identifierSeparator \
+  > $yamlDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml
+
+  cat $yamlDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml \
+  | node $rootDirectory/Processors/removeSelfDependentItemsOfType \
+    --type=variable \
+  > $yamlDirectory/remove-self-dependent-items-of-type-variable.yaml
+
+  cat $yamlDirectory/remove-self-dependent-items-of-type-variable.yaml \
+  | node $rootDirectory/Processors/orderItemsBy/indexOf/type \
+    --typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file \
+  > $yamlDirectory/order-items-by-index-of-type.yaml
+
+  cat $yamlDirectory/order-items-by-index-of-type.yaml \
+  | node $rootDirectory/Processors/createOrAddToStacks/uniformly \
+    --commaSeparatedLevels=test --commaSeparatedLevels=existing \
+  > $yamlDirectory/stack-test-in-top-level.yaml
+
+  cat $yamlDirectory/stack-test-in-top-level.yaml \
+  | node $rootDirectory/Processors/createOrAddToStacks/toItemsWithIdentifier \
+    --commaSeparatedLevels=existing --commaSeparatedLevels=expect,test \
+    --toIdentifier=test \
+  > $yamlDirectory/add-to-stack-test-identifiers.yaml
+  
+  cat $yamlDirectory/add-to-stack-test-identifiers.yaml \
+  | node $rootDirectory/Processors/unstackIndependent \
+  > $yamlDirectory/unstack-independent.yaml
+}
+
+ensureDirectoryExists $outputDirectory
+
+packages=(call-when-process-entry-point dependency-and-structure run-tests-from-file-system)
+
+# repository
+
+ensureDirectoryExists $outputDirectory/repository
 
 node $rootDirectory/javascript-analyzer/getOrCreateItemsInDirectory \
   --directory=$rootDirectory \
   --ignoreDirectoryNames=coverage --ignoreDirectoryNames=node_modules --ignoreDirectoryNames=output --ignoreDirectoryNames=test-cases --ignoreDirectoryNames=test-coverage \
-> $outputDirectory/analysis-of-repository.yaml
+> $outputDirectory/repository/analysis.yaml
 
-# analyze packages
-
-packages=(call-when-process-entry-point dependency-and-structure run-tests-from-file-system)
+processInDirectory $outputDirectory/repository
 
 sed \
   -e "s/'@devsnicket\/eunice-${packages[0]}'/${packages[0]}/g" \
   -e "s/'@devsnicket\/eunice-${packages[1]}'/${packages[1]}/g" \
   -e "s/'@devsnicket\/eunice-${packages[2]}'/${packages[2]}/g" \
-  $outputDirectory/analysis-of-repository.yaml \
-> $outputDirectory/analysis-of-repository-without-package-prefixes.yaml
+  $outputDirectory/repository/unstack-independent.yaml \
+> $outputDirectory/repository/without-package-prefixes.yaml
+
+# packages
 
 for package in ${packages[@]}; do
   npm install --no-save --prefix $outputDirectory @devsnicket/eunice-$package
+
+  ensureDirectoryExists $outputDirectory/$package
 
   node $rootDirectory/javascript-analyzer/getOrCreateItemsInDirectory \
     --directory=$outputDirectory/node_modules/@devsnicket/eunice-$package \
     --ignoreDirectoryNames=dist \
     --ignoreDirectoryNames=node_modules \
-  > $outputDirectory/analysis-of-package-$package.yaml
+  > $outputDirectory/$package/analysis.yaml
 
-  sed \
-    -e "s/^- id: /- id: $package\//g" \
-    -e "/^- id: /! s/^- /- $package\//g" \
-    $outputDirectory/analysis-of-package-$package.yaml \
-  > $outputDirectory/analysis-of-package-$package-with-root-prefix.yaml
-done
-
-# process and render this repository and packages
-
-( \
-  cat $outputDirectory/analysis-of-repository-without-package-prefixes.yaml \
-  &&
-  cat $outputDirectory/analysis-of-package-${packages[0]}-with-root-prefix.yaml \
-  &&
-  cat $outputDirectory/analysis-of-package-${packages[1]}-with-root-prefix.yaml \
-  &&
-  cat $outputDirectory/analysis-of-package-${packages[2]}-with-root-prefix.yaml \
-) \
-| node $rootDirectory/Processors/setTypeOfRootItems \
-  --type=file \
-> $outputDirectory/set-type-of-root-items-to-file.yaml
-
-cat $outputDirectory/set-type-of-root-items-to-file.yaml \
-| node $rootDirectory/Processors/removeIdentifierSuffix \
-  --suffix=/index \
-> $outputDirectory/remove-identifier-suffix-of-index.yaml
-
-cat $outputDirectory/remove-identifier-suffix-of-index.yaml \
-| node $rootDirectory/Processors/orderItemsBy/identifier \
-> $outputDirectory/order-items-by-identifier.yaml
-
-cat $outputDirectory/order-items-by-identifier.yaml \
-| node $rootDirectory/Processors/groupItemsByIdentifierSeparator \
-  --identifierSeparator=/ \
-> $outputDirectory/group-items-by-identifier-separator-of-slash.yaml
-
-cat $outputDirectory/group-items-by-identifier-separator-of-slash.yaml \
-| node $rootDirectory/Processors/removeRedundantParentIdentifierPrefix \
-  --identifierSeparator=/ \
-> $outputDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml
-
-cat $outputDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml \
-| node $rootDirectory/Processors/removeSelfDependentItemsOfType \
-  --type=variable \
-> $outputDirectory/remove-self-dependent-items-of-type-variable.yaml
-
-cat $outputDirectory/remove-self-dependent-items-of-type-variable.yaml \
-| node $rootDirectory/Processors/orderItemsBy/indexOf/type \
-  --typesInOrder= --typesInOrder=parameter --typesInOrder=variable --typesInOrder=file \
-> $outputDirectory/order-items-by-index-of-type.yaml
-
-cat $outputDirectory/order-items-by-index-of-type.yaml \
-| node $rootDirectory/Processors/createOrAddToStacks/uniformly \
-  --commaSeparatedLevels=test --commaSeparatedLevels=existing \
-> $outputDirectory/stack-test-in-top-level.yaml
-
-cat $outputDirectory/stack-test-in-top-level.yaml \
-| node $rootDirectory/Processors/createOrAddToStacks/toItemsWithIdentifier \
-  --commaSeparatedLevels=existing --commaSeparatedLevels=expect,test \
-  --toIdentifier=test \
-> $outputDirectory/add-to-stack-test-identifiers.yaml
-
-cat $outputDirectory/add-to-stack-test-identifiers.yaml \
-| node $rootDirectory/Processors/createOrAddToStacks/usingFileSystem \
-  --directory=$rootDirectory \
-> $outputDirectory/stack-using-files-in-repository.yaml
-
-lastStackUsingFiles=repository
-
-for package in ${packages[@]}; do
-  cat $outputDirectory/stack-using-files-in-$lastStackUsingFiles.yaml \
+  processInDirectory $outputDirectory/$package
+  
+  cat $outputDirectory/$package/unstack-independent.yaml \
   | node $rootDirectory/Processors/createOrAddToStacks/usingFileSystem \
     --directory=$outputDirectory/node_modules/@devsnicket/eunice-$package \
-    --subsetIdentifierHierarchy=$package \
-  > $outputDirectory/stack-using-files-in-$package.yaml
+    --subsetIdentifierHierarchy= \
+  > $outputDirectory/$package/stack-using-files.yaml
 
-  lastStackUsingFiles=$package
+  cat $outputDirectory/$package/stack-using-files.yaml \
+	| node $rootDirectory/Processors/replaceIdentifiers \
+    --pattern=.+ \
+    --replacement="$package$identifierSeparator$&" \
+    --rootOnly=true \
+	| node $rootDirectory/Processors/replaceIdentifiers \
+    --pattern=^$ \
+    --replacement=$package \
+    --rootOnly=true \
+  > $outputDirectory/$package/with-root-prefix.yaml
 done
 
-cat $outputDirectory/stack-using-files-in-$lastStackUsingFiles.yaml \
-| node $rootDirectory/Processors/unstackIndependent \
+node $rootDirectory/Processors/concatenateFromFileSystem \
+  --files $outputDirectory/repository/without-package-prefixes.yaml \
+  --files $outputDirectory/${packages[0]}/with-root-prefix.yaml \
+  --files $outputDirectory/${packages[1]}/with-root-prefix.yaml \
+  --files $outputDirectory/${packages[2]}/with-root-prefix.yaml \
+> $outputDirectory/concatenate.yaml
+  
+cat $outputDirectory/concatenate.yaml \
+| node $rootDirectory/Processors/createOrAddToStacks/usingFileSystem \
+  --directory=$rootDirectory \
 > $outputDirectory/.yaml
 
 cat $outputDirectory/.yaml \
