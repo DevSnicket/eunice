c985910f44b1a2931f376ddc71f82bd5d17ae0fe
Graham Dyson <grahamdyson@hotmail.com>
2018-12-07 18:28:56 +0000

    harness Bash scripts can be run from another directory

--- harnesses/build.sh
+++ harnesses/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-(cd javascript-analyzer-and-renderer && npm run build) \
+(cd $(dirname $0)/javascript-analyzer-and-renderer && npm run build) \
 && \
-(cd renderer && npm run build)
+(cd $(dirname $0)/renderer && npm run build)
--- harnesses/lint.sh
+++ harnesses/lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-(cd javascript-analyzer-and-renderer && npm run lint) \
+(cd $(dirname $0)/javascript-analyzer-and-renderer && npm run lint) \
 && \
-(cd renderer && npm run lint)
+(cd $(dirname $0)/renderer && npm run lint)
