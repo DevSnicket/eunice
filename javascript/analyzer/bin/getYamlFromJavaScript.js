06277d12f261c37f36efa85ed7c06f3496eab0d7
Graham Dyson <grahamdyson@hotmail.com>
2018-12-11 09:59:50 +0000

    replaced code coverage ignore in bin files with package configuration

--- javascript/analyzer/bin/getYamlFromJavaScript.js
+++ javascript/analyzer/bin/getYamlFromJavaScript.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* istanbul ignore file: only used when JavaScript file is process entry point */
 const
 	callWhenProcessEntryPoint = require("@devsnicket/eunice-call-when-process-entry-point"),
 	getYamlFromJavaScript = require("../getYamlFromJavaScript");
