4cd1afea8aed219529562f6afc419917bddfeb8c
Graham Dyson <grahamdyson@hotmail.com>
2018-12-16 21:08:19 +0000

    removed unused argument in visit function declaration

--- javascript/analyzer/getItemOrItemsFromJavaScript/createVisitors/createFunctionDeclaration.js
+++ javascript/analyzer/getItemOrItemsFromJavaScript/createVisitors/createFunctionDeclaration.js
@@ -16,7 +16,6 @@ module.exports =
 					[
 						...createParameterItemsForFunction({
 							functionDeclaration,
-							getPropertyName,
 							hasUndeclaredReferenceTo,
 						}),
 						...items || [],
