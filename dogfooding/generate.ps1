994e12a8bd858c19ae3b174f98989afb973f2b74
Graham Dyson <grahamdyson@hotmail.com>
2018-12-16 22:37:24 +0000

    dogfooding uses setIdentifierOfAnonymousToParent processor

--- dogfooding/generate.ps1
+++ dogfooding/generate.ps1
@@ -55,6 +55,11 @@ try
 		> $yamlDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml
 
 		cat $yamlDirectory/remove-redundant-parent-identifier-prefix-of-slash.yaml `
+		| npx eunice-processors-setIdentifierOfAnonymousToParent `
+			--identifierSeparator=$identifierSeparator `
+		> $yamlDirectory/set-identifier-of-anonymous-to-parent.yaml
+
+		cat $yamlDirectory/set-identifier-of-anonymous-to-parent.yaml `
 		| npx eunice-processors-removeSelfDependentItemsOfType `
 			--type=variable `
 		> $yamlDirectory/remove-self-dependent-items-of-type-variable.yaml
