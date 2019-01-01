8f3b381cd7a5125b7b79f082128ead3d1ae4793b
Graham Dyson <grahamdyson@hotmail.com>
2019-01-01 21:15:13 +0000

    added code coverage ignore for main index.js file

--- dependency-counter/index.js
+++ dependency-counter/index.js
@@ -1,3 +1,4 @@
+/* istanbul ignore file: test would be a mirror of implementation */
 const
 	getInteractiveElementsForYaml = require("./getInteractiveElementsForYaml"),
 	getSvgForYaml = require("./getSvgForYaml");
