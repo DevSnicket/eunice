7259f788aea16047cd83285609c1d21a3bcfb4e7
Graham Dyson <grahamdyson@hotmail.com>
2019-01-02 17:09:59 +0000

    added Jest describes for two test.each calls there are tests with the same name in the two calls

--- dependency-and-structure/createYamlFromStack/test.js
+++ dependency-and-structure/createYamlFromStack/test.js
@@ -2,12 +2,16 @@ const
 	createSymmetricalTestCases = require("../testcases/createSymmetricalTestCases"),
 	createYamlFromStack = require(".");
 
-test.each(
-	createSymmetricalTestCases()
-	.map(testcase => [ testcase.stackDescription, testcase.yaml, testcase.stack ]),
-)(
-	"%s returns %j",
-	(stackDescription, yaml, stack) =>
-		expect(createYamlFromStack(stack))
-		.toEqual(yaml),
+describe(
+	"createYamlFromStack",
+	() =>
+		test.each(
+			createSymmetricalTestCases()
+			.map(testcase => [ testcase.stackDescription, testcase.yaml, testcase.stack ]),
+		)(
+			"%s returns %j",
+			(stackDescription, yaml, stack) =>
+				expect(createYamlFromStack(stack))
+				.toEqual(yaml),
+		),
 );
