730d3f9c5add91b20a51a7a4a12e10453bd605d0
Graham Dyson <grahamdyson@hotmail.com>
2019-01-07 21:38:30 +0000

    orderItemsByIndexOType code dependency path corrected broken in https://github.com/DevSnicket/eunice-processors/commit/4392f1d6c6baa6550bb24c46d5e18c8a67ecf7c6

--- stacking-explicit/sorting/orderItemsByIndexOfType/bin.js
+++ stacking-explicit/sorting/orderItemsByIndexOfType/bin.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const
-	callWithProcessStandardStreamsOfYaml = require("../../../callWithProcessStandardStreamsOfYaml"),
+	callWithProcessStandardStreamsOfYaml = require("../../callWithProcessStandardStreamsOfYaml"),
 	orderItemsByType = require("./");
 
 callWithProcessStandardStreamsOfYaml(
