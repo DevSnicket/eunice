1fe83c6b6cbe348f896ec15b47c3abcefa44beb4
Graham Dyson <grahamdyson@hotmail.com>
2019-05-28 23:12:19 +0100

    removed dogfooding yaml substitution in harness javascript dogfooding is not run from this repository and the substitution will happen by using this repositories package

--- interactive/webpack.config.js
+++ interactive/webpack.config.js
@@ -1,12 +1,6 @@
 const createWebpackConfiguration = require("./createWebpackConfiguration");
 
 module.exports =
-	createWebpackConfiguration({
-		directory:
-			"dist/harness",
-		javascriptSubstitution:
-			{
-				pattern: "yamlFromWebpack",
-				replacementFilePath: "../../dogfooding/.yaml",
-			},
-	});
+	createWebpackConfiguration(
+		{ directory: "dist/harness" },
+	);
