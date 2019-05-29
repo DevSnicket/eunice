f9a6f124dec2b1dc5d45cccb87d1779eb989430d
Graham Dyson <grahamdyson@hotmail.com>
2019-05-29 10:01:59 +0100

    project is now a NPM package

---
+++ interactive/index.js
@@ -0,0 +1,12 @@
+/* istanbul ignore file: test would be a mirror of implementation */
+const
+	createWebpackConfiguration = require("./createWebpackConfiguration"),
+	createYamlInputElement = require("./createYamlInputElement"),
+	createYamlOutputElement = require("./createYamlOutputElement");
+
+module.exports =
+	{
+		createWebpackConfiguration,
+		createYamlInputElement,
+		createYamlOutputElement,
+	};
