0d2ad8055118b753f7877633cb6fc3e36c33bb12
Graham Dyson <grahamdyson@hotmail.com>
2019-06-25 15:07:05 +0100

    added per-file license comments

--- javascript/analyzer/call-with-process-standard-streams/index.js
+++ javascript/analyzer/call-with-process-standard-streams/index.js
@@ -1,3 +1,6 @@
+/* Copyright (c) 2018 Graham Dyson. All Rights Reserved.
+Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
+
 const minimist = require("minimist");
 
 module.exports =
