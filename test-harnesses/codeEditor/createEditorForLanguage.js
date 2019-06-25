712a1bac5a17eedee76831d99d86adf9fec73b67
Graham Dyson <grahamdyson@hotmail.com>
2019-06-25 15:27:39 +0100

    added per-file license comments

--- test-harnesses/codeEditor/createEditorForLanguage.js
+++ test-harnesses/codeEditor/createEditorForLanguage.js
@@ -1,3 +1,6 @@
+/* Copyright (c) 2018 Graham Dyson. All Rights Reserved.
+Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
+
 const
 	{ editor: { create: createMonacoEditor } } = require("monaco-editor"),
 	{ createElement } = require("react");
--- test-harnesses/processor-plugins/index.js
+++ test-harnesses/processor-plugins/index.js
@@ -1,1 +1,4 @@
+/* Copyright (c) 2018 Graham Dyson. All Rights Reserved.
+Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
+
 module.exports = require("@devsnicket/plugin-discovery-create-repository")();
