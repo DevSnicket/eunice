7ae9759e7406fded1619032f331d52dd245addd7
Graham Dyson <grahamdyson@hotmail.com>
2019-07-03 12:16:48 +0100

    added per-file license comments changed license to GPL 3+ specified breaking in package version due to license change

--- stacking-explicit/groupItemsByIdentifierSeparator/getItemOrCreateItemForGroupAndParents.js
+++ stacking-explicit/groupItemsByIdentifierSeparator/getItemOrCreateItemForGroupAndParents.js
@@ -1,3 +1,6 @@
+/* Copyright (c) 2019 Graham Dyson. All Rights Reserved.
+This library is free software, licensed under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 const getItemOrCreateItemForGroup = require("./getItemOrCreateItemForGroup");
 
 module.exports = getItemOrCreateItemForGroupAndParents;
--- stacking-explicit/setIdentifierOfAnonymousToParent/bin.js
+++ stacking-explicit/setIdentifierOfAnonymousToParent/bin.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+/* Copyright (c) 2019 Graham Dyson. All Rights Reserved.
+This library is free software, licensed under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 const
 	callWithProcessStandardStreamsOfYaml = require("../callWithProcessStandardStreamsOfYaml"),
 	setIdentifierOfAnonymousToParent = require("./");
