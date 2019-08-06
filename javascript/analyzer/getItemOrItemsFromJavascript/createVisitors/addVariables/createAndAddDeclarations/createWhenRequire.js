92440ed2d8c9eaf973529590485d3c193f9cacf0
Graham Dyson <grahamdyson@hotmail.com>
2019-08-06 08:06:44 +0100

    ".js" removed from CommonJS require file paths matches existing files in directory behaviour

--- javascript/analyzer/getItemOrItemsFromJavascript/createVisitors/addVariables/createAndAddDeclarations/createWhenRequire.js
+++ javascript/analyzer/getItemOrItemsFromJavascript/createVisitors/addVariables/createAndAddDeclarations/createWhenRequire.js
@@ -1,6 +1,8 @@
 /* Copyright (c) 2018 Graham Dyson. All Rights Reserved.
 This library is free software, licensed under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
+const removeJsFilePathExtension = require("../../removeJsFilePathExtension");
+
 module.exports =
 	({
 		createVariablesFromIdentifier,
@@ -76,7 +78,11 @@ module.exports =
 				);
 
 				function getArgument() {
-					return expression.arguments[0].value;
+					return (
+						removeJsFilePathExtension(
+							expression.arguments[0].value,
+						)
+					);
 				}
 			}
 
