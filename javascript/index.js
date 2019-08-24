d80bccea533d096f238fee114d604d39f67f4d20
Graham Dyson <git@devsnicket.com>
2019-08-24 16:20:31 +0100

    replaced packages main/root index.js with bin without shebang

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,15 @@
 This library is free software, licensed under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 /* istanbul ignore file: test would be a mirror of implementation */
-const analyzeAndProcessAndRender = require("./analyzeAndProcessAndRender");
+const
+	analyzeAndProcessAndRender = require("./analyzeAndProcessAndRender"),
+	createParameterFromCliArguments = require("./createParameterFromCliArguments"),
+	minimist = require("minimist");
 
-module.exports = { analyzeAndProcessAndRender };
+analyzeAndProcessAndRender(
+	createParameterFromCliArguments(
+		minimist(
+			process.argv.slice(2),
+		),
+	),
+);
