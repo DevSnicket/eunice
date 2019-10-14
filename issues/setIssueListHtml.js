3b73f7699a2dba1a002a7597bb7d2ee60af78801
Graham Dyson <git@devsnicket.com>
2019-10-14 11:58:06 +0100

    JavaScript release list is dynamically generated in the browser

---
+++ issues/setIssueListHtml.js
@@ -0,0 +1,71 @@
+import "https://unpkg.com/array-flat-polyfill";
+
+fetch("https://api.github.com/repos/devsnicket/eunice/issues")
+.then(response => response.json())
+.then(setHtmlFromIssues);
+
+function setHtmlFromIssues(
+	issues,
+) {
+	setIssueListHtml(
+		getHtmlForIssues(
+			orderIssuesByNumber(
+				getIssuesByNumber(),
+			),
+		),
+	);
+
+	function getIssuesByNumber() {
+		return (
+			new Map(
+				issues.map(issue => [ issue.number, issue ]),
+			)
+		);
+	}
+}
+
+function orderIssuesByNumber(
+	issuesByNumber,
+) {
+	return (
+		[
+			...getOrdered(),
+			...issuesByNumber.values(),
+		]
+	);
+
+	function getOrdered() {
+		return (
+			[ 53, 55, 43, 51, 52, 50, 45, 38, 37, 47, 16, 48, 49, 44, 46, 56, 42, 12, 14 ]
+			.flatMap(popWithNumber)
+		);
+
+		function popWithNumber(
+			number,
+		) {
+			const issue = issuesByNumber.get(number);
+
+			issuesByNumber.delete(number);
+
+			return issue || [];
+		}
+	}
+}
+
+function getHtmlForIssues(
+	issues,
+) {
+	return issues.map(getHtmlForIssue).join("");
+}
+
+function getHtmlForIssue(
+	issue,
+) {
+	return `<div><a href="${issue.html_url}" target="_blank">${issue.title}</a></div>`;
+}
+
+function setIssueListHtml(
+	html,
+) {
+	document.getElementById("issues-list").innerHTML = html;
+}
---
+++ javascript/releases/setReleasesListHtml.js
@@ -0,0 +1,169 @@
+import "https://unpkg.com/array-flat-polyfill";
+
+fetchJson("https://api.github.com/search/issues?q=released%20in%20javascript+repo:devsnicket/eunice+in:comments")
+.then(setHtmlFromSearchResults);
+
+function setHtmlFromSearchResults({
+	items,
+}) {
+	Promise.all(items.map(addVersionToIssue))
+	.then(
+		issues =>
+			setListHtml(
+				getHtmlForReleases(
+					getReleasesForIssues(issues)
+					.sort(compareRelease),
+				),
+			)
+	);
+}
+
+function addVersionToIssue({
+	comments_url,
+	html_url,
+	title,
+}) {
+	return (
+		fetchJson(comments_url)
+		.then(
+			comments => (
+				{
+					html_url,
+					title,
+					version: getVersionFromComments(comments),
+				}
+			),
+		)
+	);
+}
+
+function fetchJson(
+	url,
+) {
+	return fetch(url).then(response => response.json());
+}
+
+function getVersionFromComments(
+	comments,
+) {
+	return (
+		comments.reduce(
+			(version, comment) =>
+				version
+				||
+				getVersionFromCommentWhenRelease(comment),
+			null,
+		)
+	);
+}
+
+function getVersionFromCommentWhenRelease({
+	body,
+}) {
+	const match = body.match(/released in \[JavaScript (?<text>(?<major>[0-9]*)\.(?<minor>[0-9]*)\.(?<patch>[0-9]*))/);
+
+	return match && createVersionFromMatchGroups(match.groups);
+}
+
+function createVersionFromMatchGroups({
+	major,
+	minor,
+	patch,
+	text,
+}) {
+	return (
+		{
+			major: Number(major),
+			minor: Number(minor),
+			patch: Number(patch),
+			text,
+		}
+	);
+}
+
+function getReleasesForIssues(
+	issues,
+) {
+	const releasesByVersionText = new Map();
+
+	for (const issue of issues) {
+		releasesByVersionText.set(
+			issue.version.text,
+			{
+				issues: getIssuesOfVersion(issue),
+				version: issue.version,
+			},
+		);
+	}
+
+	return [ ...releasesByVersionText.values() ];
+
+	function getIssuesOfVersion(
+		issue,
+	) {
+		const release = releasesByVersionText.get(issue.version.text);
+
+		return (
+			[
+				... (release && release.issues) || [],
+				issue,
+			]
+		);
+	}
+}
+
+function compareRelease(
+	left,
+	right,
+) {
+	return compareVersion(left.version, right.version);
+}
+
+function compareVersion(
+	left,
+	right
+) {
+	return (
+		right.major - left.major
+		||
+		right.minor - left.minor
+		||
+		right.patch - left.patch
+	);
+}
+
+function getHtmlForReleases(
+	releases,
+) {
+	return releases.map(getHtmlForRelease).join("");
+}
+
+function getHtmlForRelease({
+	issues,
+	version
+}) {
+	return (
+		getHtmlForVersion(version)
+		+
+		issues.map(getHtmlForIssue).join("")
+	);
+}
+
+function getHtmlForVersion({
+	text,
+}) {
+	return `<h3 id="${text}">${text}</h3>`;
+}
+
+function getHtmlForIssue({
+	html_url,
+	title,
+}) {
+	return `<div><a href="${html_url}" target="_blank">${title}</a></div>`;
+}
+
+function setListHtml(
+	html,
+) {
+	document.getElementById("releases-list").innerHTML = html;
+}
