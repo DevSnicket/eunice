a066c5a3909749aefc90ea4bf9a1b29d2ad99be5
Graham Dyson <git@devsnicket.com>
2019-10-21 20:11:43 +0100

    corrected case of array flatmap require previous commits failed continuous integration and packages weren't released

--- run-tests-from-file-system/discoverTestCases.js
+++ run-tests-from-file-system/discoverTestCases.js
@@ -1,7 +1,7 @@
 /* Copyright (c) 2019 Graham Dyson. All Rights Reserved.
 Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
 
-require("array.prototype.flatMap")
+require("array.prototype.flatmap")
 .shim();
 
 const
