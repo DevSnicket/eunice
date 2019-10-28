d2c647757eed2933b9d9da688086acd43b8c6459
Graham Dyson <git@devsnicket.com>
2019-10-28 08:56:27 +0000

    tests for multiple module exports

---
+++ javascript/analyzer/getYamlFromJavascript/tests/default/test-cases/modules/export/function/named/binding/function-declaration/after/multiple/exports/.js
@@ -0,0 +1,4 @@
+function exported1() {}
+function exported2() {}
+
+export { exported1, exported2 }
---
+++ javascript/analyzer/getYamlFromJavascript/tests/default/test-cases/modules/export/function/named/binding/function-declaration/after/multiple/specifiers/.js
@@ -0,0 +1,5 @@
+function exported1() {}
+function exported2() {}
+
+export { exported1 }
+export { exported2 }
