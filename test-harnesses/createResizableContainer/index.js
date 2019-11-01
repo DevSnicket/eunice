5df2cebdb83567d3563fc43441aacc356fae8a97
Graham Dyson <git@devsnicket.com>
2019-11-01 11:54:01 +0000

    horizontal resizable containers previously only available as vertical DevSnicket/eunice#86

---
+++ test-harnesses/createResizableContainer/index.js
@@ -0,0 +1,154 @@
+/* Copyright (c) 2018 Graham Dyson. All Rights Reserved.
+Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
+
+require("array.prototype.flatmap")
+.shim();
+
+const createSplitterIcon = require("./createSplitterIcon");
+
+require("react-reflex/styles.css");
+require("./index.css");
+
+module.exports =
+	({
+		createElement,
+		items,
+		flexKeysAndValues,
+		orientation,
+		resizableElementTypes,
+	}) =>
+		createElement(
+			resizableElementTypes.container,
+			{ orientation },
+			createContainerChildElements({
+				createElement,
+				flexKeysAndValues,
+				itemElementType:
+					resizableElementTypes.item,
+				items,
+				splitter:
+					{
+						elementType:
+							resizableElementTypes.splitter,
+						icon:
+							createSplitterIcon({
+								createElement,
+								orientation,
+							}),
+					},
+			}),
+		);
+
+function createContainerChildElements({
+	createElement,
+	flexKeysAndValues,
+	itemElementType,
+	items,
+	splitter,
+}) {
+	return (
+		items.flatMap(
+			withContext({
+				createElement,
+				createReflexSplitterForIndex:
+					index =>
+						createSplitterWhenSubsequent({
+							createElement,
+							index,
+							...splitter,
+						}),
+				flexKeysAndValues,
+				itemElementType,
+			})
+			.createSplitterAndElementForItemAndIndex,
+		)
+	);
+}
+
+function withContext({
+	createElement,
+	createReflexSplitterForIndex,
+	flexKeysAndValues: { getValueOfKey, setKeyAndValue },
+	itemElementType,
+}) {
+	return { createSplitterAndElementForItemAndIndex };
+
+	function createSplitterAndElementForItemAndIndex(
+		{ element, flex },
+		index,
+	) {
+		return (
+			[
+				...createReflexSplitterForIndex(index),
+				createItemElement(),
+			]
+		);
+
+		function createItemElement() {
+			return (
+				createElement(
+					itemElementType,
+					{
+						...createFlexProperty(),
+						key:
+							`element ${index}`,
+						onStopResize:
+							createOnStopResize(),
+					},
+					element,
+				)
+			);
+		}
+
+		function createFlexProperty() {
+			return (
+				flex
+				&&
+				createPropertyWhenHasValue(
+					(flex.key && getValueOfKey(flex.key))
+					||
+					flex.default,
+				)
+			);
+
+			function createPropertyWhenHasValue(
+				value,
+			) {
+				return value && { flex: value };
+			}
+		}
+
+		function createOnStopResize() {
+			return (
+				flex && flex.key
+				&&
+				onStopResize
+			);
+
+			function onStopResize(
+				{ component },
+			) {
+				setKeyAndValue({
+					key: flex.key,
+					value: component.props.flex,
+				});
+			}
+		}
+	}
+}
+
+function * createSplitterWhenSubsequent({
+	createElement,
+	elementType,
+	icon,
+	index,
+}) {
+	if (index > 0)
+		yield (
+			createElement(
+				elementType,
+				{ key: `splitter ${index - 1}` },
+				icon,
+			)
+		);
+}
---
+++ test-harnesses/tests/removeWhitespaceFromTestExpected.js
@@ -0,0 +1,4 @@
+/* Copyright (c) 2019 Graham Dyson. All Rights Reserved.
+Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
+
+module.exports = expected => expected.replace(/\n|\t/g, "");
