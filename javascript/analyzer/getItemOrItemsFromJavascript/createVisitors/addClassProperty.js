DevSnicket/eunice

commit 55a3ecbaf51b74b02dba06f3ce58d2b8786021f7

dependencies associated with class fields DevSnicket/eunice#92

javascript/analyzer/getItemOrItemsFromJavascript/createVisitors/addClassProperty.js
@@ -1,0 +1,37 @@
+// Copyright (c) 2019 Graham Dyson. All Rights Reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+
+module.exports =
+	({
+		ancestors,
+		addDeclarationIn,
+		classProperty,
+		createDependsUponProperty,
+	}) => {
+		if (!isFunction())
+			addDeclarationIn({
+				declaration: createDeclaration(),
+				parent: getParentClass(),
+			});
+
+		function isFunction() {
+			return (
+				[ "ArrowFunctionExpression", "FunctionExpression" ]
+				.includes(classProperty.value.type)
+			);
+		}
+
+		function createDeclaration() {
+			return (
+				{
+					id: classProperty.key.name,
+					...createDependsUponProperty(
+						{ parent: classProperty },
+					),
+				}
+			);
+		}
+
+		function getParentClass() {
+			return ancestors[ancestors.length - 3];
+		}
+	};

javascript/analyzer/getYamlFromJavascript/tests/classFields/test-cases/literal/static/.js
@@ -1,0 +1,3 @@
+class Class {
+	static field = false;
+}

javascript/analyzer/getYamlFromJavascript/tests/classFields/test-cases/call/.js
@@ -1,0 +1,3 @@
+class Class {
+	field = called();
+}

javascript/analyzer/getYamlFromJavascript/tests/classFields/test-cases/call/.yaml
@@ -1,0 +1,4 @@
+- id: Class
+  items:
+    id: field
+    dependsUpon: called

javascript/analyzer/getYamlFromJavascript/tests/classFields/test-cases/call/in-object/.js
@@ -1,0 +1,3 @@
+class Class {
+	field = { property: called() }
+}

commit cff0cae419bc208f7c692b924f3603991d791dcf

unassigned class fields no longer raise an error introduced in 55a3ecb DevSnicket/eunice#94

javascript/analyzer/getItemOrItemsFromJavascript/createVisitors/addClassProperty.js
@@ -7,18 +7,11 @@ module.exports =
 		classProperty,
 		createDependsUponProperty,
 	}) => {
-		if (!isFunction())
+		if (!isFunction(classProperty))
 			addDeclarationIn({
 				declaration: createDeclaration(),
 				parent: getParentClass(),
 			});
-
-		function isFunction() {
-			return (
-				[ "ArrowFunctionExpression", "FunctionExpression" ]
-				.includes(classProperty.value.type)
-			);
-		}
 
 		function createDeclaration() {
 			return (
@@ -35,3 +28,14 @@ module.exports =
 			return ancestors[ancestors.length - 3];
 		}
 	};
+
+function isFunction(
+	{ value },
+) {
+	return (
+		value
+		&&
+		[ "ArrowFunctionExpression", "FunctionExpression" ]
+		.includes(value.type)
+	);
+}

javascript/analyzer/getYamlFromJavascript/tests/classFields/test-cases/.yaml
@@ -1,0 +1,2 @@
+- id: Class
+  items: field

javascript/analyzer/getYamlFromJavascript/tests/classFields/test-cases/instance/.js
@@ -1,0 +1,3 @@
+class Class {
+	field;
+}

javascript/analyzer/getYamlFromJavascript/tests/classFields/test-cases/literal/static/.js
@@ -1,3 +1,0 @@
-class Class {
-	static field = false;
-}

javascript/analyzer/getYamlFromJavascript/tests/classFields/test-cases/static/.js
@@ -1,0 +1,3 @@
+class Class {
+	static field;
+}