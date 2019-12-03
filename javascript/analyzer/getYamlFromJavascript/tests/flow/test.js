2ff1b59d54088f22a50a87eb9325c1a885c6f999
Graham Dyson <git@devsnicket.com>
2019-12-03 09:18:07 +0000

    tests added for Flow export and import

---
+++ javascript/analyzer/getYamlFromJavascript/tests/flow/test.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2018 Graham Dyson. All Rights Reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+
+const
+	getYamlFromJavascript = require("../.."),
+	path = require("path"),
+	runTestsFromFileSystem = require("@devsnicket/eunice-run-tests-from-file-system");
+
+runTestsFromFileSystem({
+	caseFileName:
+		".js",
+	directoryAbsolutePath:
+		path.join(__dirname, "test-cases"),
+	expectedFileName:
+		".yaml",
+	getActualForTestCase:
+		({ content }) =>
+			getYamlFromJavascript({
+				babelParserPlugins: [ "flow" ],
+				javascript: content,
+			}),
+	processArguments:
+		process.argv,
+});
