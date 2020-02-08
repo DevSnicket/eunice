539771600c3c1e0a4155bea9a0b42941b69fabe1
Graham Dyson <git@devsnicket.com>
2020-02-08 07:08:03 +0000

    switched from CommonJS to modules

--- javascript/analyzer/call-with-process-standard-streams/index.js
+++ javascript/analyzer/call-with-process-standard-streams/index.js
@@ -1,52 +1,51 @@
 /* Copyright (c) 2018 Graham Dyson. All Rights Reserved.
 Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
 
-const minimist = require("minimist");
+import minimist from "minimist";
 
-module.exports =
-	({
-		action,
-		standardInputParameter = null,
-	}) => {
-		const processArguments = minimist(process.argv.slice(2));
+export default ({
+	action,
+	standardInputParameter = null,
+}) => {
+	const processArguments = minimist(process.argv.slice(2));
 
-		if (standardInputParameter && !processArguments[standardInputParameter])
-			callWithProcessArgumentsAndStandardInputStream();
-		else
-			call(processArguments);
+	if (standardInputParameter && !processArguments[standardInputParameter])
+		callWithProcessArgumentsAndStandardInputStream();
+	else
+		call(processArguments);
 
-		function callWithProcessArgumentsAndStandardInputStream() {
-			const standardIn = process.stdin;
+	function callWithProcessArgumentsAndStandardInputStream() {
+		const standardIn = process.stdin;
 
-			standardIn.resume();
-			standardIn.setEncoding("utf8");
+		standardIn.resume();
+		standardIn.setEncoding("utf8");
 
-			let input = "";
+		let input = "";
 
-			standardIn.on(
-				"data",
-				chunk => input += chunk,
-			);
+		standardIn.on(
+			"data",
+			chunk => input += chunk,
+		);
 
-			standardIn.on(
-				"end",
-				() =>
-					call({
-						...processArguments,
-						[standardInputParameter]: input,
-					}),
-			);
-		}
+		standardIn.on(
+			"end",
+			() =>
+				call({
+					...processArguments,
+					[standardInputParameter]: input,
+				}),
+		);
+	}
 
-		function call(
-			processArgumentsAndOrStandardInput,
-		) {
-			Promise.resolve(
-				action(processArgumentsAndOrStandardInput),
-			)
-			.then(
-				// eslint-disable-next-line no-console
-				console.log,
-			);
-		}
-	};
+	function call(
+		processArgumentsAndOrStandardInput,
+	) {
+		Promise.resolve(
+			action(processArgumentsAndOrStandardInput),
+		)
+		.then(
+			// eslint-disable-next-line no-console
+			console.log,
+		);
+	}
+};
