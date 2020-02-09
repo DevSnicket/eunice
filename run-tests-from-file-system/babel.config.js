5bf27211f2ed6fe5c3821913251578ee89a90c2a
Graham Dyson <git@devsnicket.com>
2020-02-09 07:51:22 +0000

    moved Babel config for Jest into package.json

--- run-tests-from-file-system/babel.config.js
+++
@@ -1,5 +0,0 @@
-/* Copyright (c) 2020 Graham Dyson. All Rights Reserved.
-Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
-
-// required for Jest
-module.exports = { plugins: [ "@babel/plugin-transform-modules-commonjs" ] };
