83259e932a62bff3b50c4e8ed358008a2cf3cc84
Graham Dyson <git@devsnicket.com>
2020-02-17 08:46:08 +0000

    fixed export of defaults in main/index of package

--- dependency-and-structure/index.js
+++ dependency-and-structure/index.js
@@ -1,7 +1,15 @@
 // Copyright (c) 2020 Graham Dyson. All Rights Reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
 
-export * from "./createStackFromYaml";
-export * from "./createYamlFromStack";
-export * from "./findDirectionBetweenItemsInFirstMutualStack";
-export * from "./findItemInStackWithIdentifierHierarchy";
-export * from "./isInnerStack";
+import createStackFromYaml from "./createStackFromYaml";
+import createYamlFromStack from "./createYamlFromStack";
+import findDirectionBetweenItemsInFirstMutualStack from "./findDirectionBetweenItemsInFirstMutualStack";
+import findItemInStackWithIdentifierHierarchy from "./findItemInStackWithIdentifierHierarchy";
+import isInnerStack from "./isInnerStack";
+
+export {
+	createStackFromYaml,
+	createYamlFromStack,
+	findDirectionBetweenItemsInFirstMutualStack,
+	findItemInStackWithIdentifierHierarchy,
+	isInnerStack,
+};
