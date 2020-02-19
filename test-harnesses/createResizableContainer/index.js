ab10a69468d1b2a1fd1614087daf67d395fb749e
Graham Dyson <git@devsnicket.com>
2020-02-19 07:56:26 +0000

    add missing array flatmap polyfill used core-js instead of array.prototype.flatmap

--- test-harnesses/createResizableContainer/index.js
+++ test-harnesses/createResizableContainer/index.js
@@ -1,6 +1,7 @@
 /* Copyright (c) 2018 Graham Dyson. All Rights Reserved.
 Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
 
+import "core-js/features/array/flat-map";
 import "react-reflex/styles.css";
 import "./index.css";
 
