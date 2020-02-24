0b9ebdb7fe1f2bdc0dbaa80696cb69b280bcfdbb
Graham Dyson <git@devsnicket.com>
2020-02-24 19:31:38 +0000

    standardised Jest Babel config for ES modules in other DevSnicket packages

---
+++ stacking-explicit/babel.config.cjs
@@ -0,0 +1,3 @@
+// Copyright (c) 2019 Graham Dyson. All Rights Reserved. Licensed under the MIT license. See LICENSE file in the repository root for full license information.
+
+module.exports = { plugins: [ "@babel/plugin-transform-modules-commonjs" ] };
