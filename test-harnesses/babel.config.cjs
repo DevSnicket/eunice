0a798ebed3c6e05aaa2a94e23df04c59ddf86259
Graham Dyson <git@devsnicket.com>
2020-02-24 20:03:35 +0000

    standardised Jest Babel config for ES modules in other DevSnicket packages

---
+++ test-harnesses/babel.config.cjs
@@ -0,0 +1,4 @@
+/* Copyright (c) 2020 Graham Dyson. All Rights Reserved.
+Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
+
+module.exports = { plugins: [ "@babel/plugin-transform-modules-commonjs" ] };
