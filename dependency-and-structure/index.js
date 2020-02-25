c9dba9027d26841385f35f30a6ddc53cedeb388a
Graham Dyson <git@devsnicket.com>
2020-02-25 10:42:34 +0000

    replace import/re-export with export defaullt from

--- dependency-and-structure/index.js
+++ dependency-and-structure/index.js
@@ -1,15 +1,7 @@
 // Copyright (c) 2020 Graham Dyson. All Rights Reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
 
-import createStackFromYaml from "./createStackFromYaml";
-import createYamlFromStack from "./createYamlFromStack";
-import findDirectionBetweenItemsInFirstMutualStack from "./findDirectionBetweenItemsInFirstMutualStack";
-import findItemInStackWithIdentifierHierarchy from "./findItemInStackWithIdentifierHierarchy";
-import isInnerStack from "./isInnerStack";
-
-export {
-	createStackFromYaml,
-	createYamlFromStack,
-	findDirectionBetweenItemsInFirstMutualStack,
-	findItemInStackWithIdentifierHierarchy,
-	isInnerStack,
-};
+export { default as createStackFromYaml } from "./createStackFromYaml";
+export { default as createYamlFromStack } from "./createYamlFromStack";
+export { default as findDirectionBetweenItemsInFirstMutualStack } from "./findDirectionBetweenItemsInFirstMutualStack";
+export { default as findItemInStackWithIdentifierHierarchy } from "./findItemInStackWithIdentifierHierarchy";
+export { default as isInnerStack } from "./isInnerStack";
