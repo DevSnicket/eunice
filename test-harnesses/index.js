ca0c9ef314e108ea2216b457564f265e0ffc4833
Graham Dyson <git@devsnicket.com>
2020-02-25 11:16:16 +0000

    replace import/re-export with export defaullt from

--- test-harnesses/index.js
+++ test-harnesses/index.js
@@ -1,22 +1,11 @@
 /* Copyright (c) 2018 Graham Dyson. All Rights Reserved.
 Licensed under the MIT license. See LICENSE file in the repository root for full license information. */
 
-import callOrCreateElementOnError from "./callOrCreateElementOnError";
-import createFillWithTitleElement from "./createFillWithTitleElement";
-import createHashFromLocation from "./createHashFromLocation";
-import createResizableContainer from "./createResizableContainer";
-import createTextareaElement from "./createTextareaElement";
-import createWebpackConfiguration from "./createWebpackConfiguration";
-import getStringLiteral from "./getStringLiteral";
-import renderIntoContainerElement from "./renderIntoContainerElement";
-
-export {
-	callOrCreateElementOnError,
-	createFillWithTitleElement,
-	createHashFromLocation,
-	createResizableContainer,
-	createTextareaElement,
-	createWebpackConfiguration,
-	getStringLiteral,
-	renderIntoContainerElement,
-};
+export { default as callOrCreateElementOnError } from "./callOrCreateElementOnError";
+export { default as createFillWithTitleElement } from "./createFillWithTitleElement";
+export { default as createHashFromLocation } from "./createHashFromLocation";
+export { default as createResizableContainer } from "./createResizableContainer";
+export { default as createTextareaElement } from "./createTextareaElement";
+export { default as createWebpackConfiguration } from "./createWebpackConfiguration";
+export { default as getStringLiteral } from "./getStringLiteral";
+export { default as renderIntoContainerElement } from "./renderIntoContainerElement";
