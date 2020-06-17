fa81d52e0242c2bc8451e8ce999631b5835547d5
Graham Dyson <git@devsnicket.com>
2020-06-17 21:02:54 +0100

    spellcheck of Git log in CI

--- csharp/push.sh
+++ csharp/push.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-./spellcheck.sh
 ./clean.sh
 ./AnalyzeProjectOrSolutionPath/Tests/TestCases/restore.sh
 ./test.sh
 ./git-log-write-file.sh
+./spellcheck.sh
 git push $1
--- csharp/spellcheck.sh
+++ csharp/spellcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f \( -name "*.fs" -o -name "*.txt" \) ! -path "./.store/**" ! -path "**/obj/**" -exec npx cspell@4.0.30 {} +
+find . -type f \( \( -name "*.fs"  ! -path "./.store/**" ! -path "./website/**" ! -path "**/obj/**" \) -o -path ./website/csharp/git-log.txt \) -exec npx cspell@4.0.30 {} +
