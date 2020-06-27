6b9a3c5ee51427b1409458e0b3a0c0d33b7a061f
Graham Dyson <git@devsnicket.com>
2020-06-27 12:15:37 +0100

    updated with rename of interactive package previously renderer-test-harness

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,1 +1,10 @@
-find . -type f -name "*.html" ! -name eunice.html ! -path ./case-studies/eunice.html ! -path ./dogfooding/index.html ! -path ./javascript/analyzer-harness/index.html ! -path ./javascript/harness/index.html ! -path ./renderer-test-harness/index.html -exec npx cspell@4.0.30 {} +
+find . \
+	-type f \
+	-name "*.html" \
+	! -name eunice.html \
+	! -path ./case-studies/eunice.html \
+	! -path ./dogfooding/index.html \
+	! -path ./interactive/index.html \
+	! -path ./javascript/analyzer-harness/index.html \
+	! -path ./javascript/harness/index.html \
+	-exec npx cspell@4.0.30 {} +
