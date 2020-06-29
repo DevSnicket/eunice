c22a975932a961d64d53dfc8301345016b9fedc6
Graham Dyson <git@devsnicket.com>
2020-06-29 10:09:12 +0100

    Git log limited to commits before 1.0.0

--- csharp/git-log-write-file.sh
+++ csharp/git-log-write-file.sh
@@ -6,4 +6,5 @@ mkdir website/csharp
 git log \
 	--date=short \
 	--pretty='format:---%n%ad%n%B' \
+	1324ddb8213e2524dbacf93d39e9a792ffaafc77..d58051c47731e2ff0da15afa87cee7ba53e9c155 \
 	> website/csharp/git-log.txt
