085b208d1b3b6940ed195222fdf93ec64a5aa657
Graham Dyson <git@devsnicket.com>
2020-06-29 13:43:09 +0100

    more specific package pack and push script paths

--- csharp/package/pack.sh
+++ csharp/package/pack.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(dirname "$0")
+
 dotnet pack \
-	DevSnicket.Eunice.fsproj \
+	../DevSnicket.Eunice.fsproj \
 	-c Release
--- csharp/package/push.sh
+++ csharp/package/push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd $(dirname "$0")
+
 dotnet nuget push \
-	bin/Release/eunice.*.nupkg \
+	../bin/Release/eunice.*.nupkg \
 	-k oy2fu5oaedz2xsna7lnqiv2i6bi5iy3uhmmvssybe53s2u \
 	-s https://api.nuget.org/v3/index.json \
 	--skip-duplicate
