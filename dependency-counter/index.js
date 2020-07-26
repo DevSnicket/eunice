cd964ed82438bcba0204dc01d7c6e5293e9625dd
Graham Dyson <git@devsnicket.com>
2020-07-26 18:54:27 +0100

    initialized parent references DevSnicket/eunice#50

--- dependency-counter/index.js
+++ dependency-counter/index.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2018 Graham Dyson. All Rights Reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
 
 import addDirectionAndStackOfDependenciesToStack from "./addDirectionAndStackOfDependenciesToStack";
+import initializeParentReferencesInStack from "@devsnicket/eunice-dependency-and-structure/initializeParentReferencesInStack";
 import replaceDirectionAndStackWithCountOfDependenciesInStack from "./replaceDirectionAndStackWithCountOfDependenciesInStack";
 
 /**
@@ -8,9 +9,15 @@ import replaceDirectionAndStackWithCountOfDependenciesInStack from "./replaceDir
   * @returns {import("./Stack.d")}
   */
 export default
-stack =>
-	replaceDirectionAndStackWithCountOfDependenciesInStack(
-		addDirectionAndStackOfDependenciesToStack(
-			stack,
-		),
-	);
+stack => {
+	const stackReplacement =
+		replaceDirectionAndStackWithCountOfDependenciesInStack(
+			addDirectionAndStackOfDependenciesToStack(
+				stack,
+			),
+		);
+
+	initializeParentReferencesInStack(stackReplacement);
+
+	return stackReplacement;
+};
