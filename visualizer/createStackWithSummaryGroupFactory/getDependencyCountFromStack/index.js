80d7e81c64f458e4c0ffaa13631381da388ae836
Graham Dyson <git@devsnicket.com>
2020-07-27 07:42:23 +0100

    subsets of one item don't show redundant summary total summary total will always be the same as the single item DevSnicket/eunice#50

--- visualizer/createStackWithSummaryGroupFactory/getDependencyCountFromStack/index.js
+++ visualizer/createStackWithSummaryGroupFactory/getDependencyCountFromStack/index.js
@@ -5,14 +5,45 @@ import "core-js/features/array/flat";
 import sumDependencyCountOfItems from "./sumDependencyCountOfItems";
 
 export default
-stack =>
-	whenHasParent(stack)
-	||
-	sumDependencyCountOfItems(
-		stack.flat(2),
+stack => {
+	return (
+		hasMultipleItems()
+		&&
+		getOrSum()
 	);
 
-function whenHasParent(
+	function hasMultipleItems() {
+		return (
+			hasMultipleLevels()
+			||
+			hasSingleLevelOfMultipleItems()
+		);
+
+		function hasMultipleLevels() {
+			return stack.length > 1;
+		}
+
+		function hasSingleLevelOfMultipleItems() {
+			return (
+				stack.length === 1
+				&&
+				stack[0].length > 1
+			);
+		}
+	}
+
+	function getOrSum() {
+		return (
+			getFromWhenHasParent(stack)
+			||
+			sumDependencyCountOfItems(
+				stack.flat(2),
+			)
+		);
+	}
+};
+
+function getFromWhenHasParent(
 	{ parent },
 ) {
 	return (
