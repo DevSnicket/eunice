a54a468654badc16a82bd1157acf977251fbd493
Graham Dyson <git@devsnicket.com>
2020-07-28 08:16:39 +0100

    svg output option includes dependency counts introduced in 81f7440ab2a82a6e7db3a3d035437a0fb701a0a9 DevSnicket/eunice#143

--- javascript/analyzeAndProcessAndRender/getSvgForYaml.js
+++ javascript/analyzeAndProcessAndRender/getSvgForYaml.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2018 Graham Dyson. All Rights Reserved. Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
 
+import countDependenciesInStack from "@devsnicket/eunice-dependency-counter";
 import { createElement } from "react";
 import { createStackFromYaml } from "@devsnicket/eunice-dependency-and-structure";
 import getSvgElementForStack from "@devsnicket/eunice-visualizer";
@@ -16,11 +17,22 @@ yaml =>
 			stack:
 				yaml
 				&&
-				createStackFromYaml(
-					// @ts-ignore
-					parseYaml(
-						yaml,
-					),
-				),
+				createStackWithDependencyCountsFromYaml(yaml),
 		}),
 	);
+
+function createStackWithDependencyCountsFromYaml(
+	yaml,
+) {
+	const stack =
+		createStackFromYaml(
+			// @ts-ignore
+			parseYaml(
+				yaml,
+			),
+		);
+
+	countDependenciesInStack(stack);
+
+	return stack;
+}
