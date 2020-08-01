8e42698c052d0a566e8cc5ae5d7ff13509b3f48b
Graham Dyson <grahamdyson@hotmail.com>
2020-08-01 10:42:55 +0100

    test case project restore/build in parallel

--- csharp/Tests/TestCases/build-restore.sh
+++ csharp/Tests/TestCases/build-restore.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 
 set -e
-
 cd $(dirname "$0")
-
-echo build
-echo
-
-find . -type f \
-	\( \
-		-path "./MembersNone/*.csproj" \
-		-o \
-		-path "./MembersLevel/Valid/*.csproj" \
-	\) \
-	\( -exec dotnet build {} --nologo \; -o -quit \)
-
-echo
-echo restore
-echo
-
-find . -type f \
-	-path "./MembersLevel/Invalid/TargetFrameworkSupported/*.csproj" \
-	\( -exec dotnet restore {} \; -o -quit \)
-
-find . -type f \
-	-path "./MembersLevel/Invalid/TargetFrameworkNotSupported/*.csproj" \
-	\( -exec dotnet restore {} -p:targetframeworks=netstandard2.0 \; -o -quit \)
+dotnet fsi --exec writeSolution.fsx
+dotnet build build.sln
+dotnet restore restore-target-framework-supported.sln
+dotnet restore restore-target-framework-not-supported.sln -p:targetframeworks=netstandard2.0
