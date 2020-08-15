cfc6052f33dd654ac584c5630347ea6a0aecc47a
Graham Dyson <grahamdyson@hotmail.com>
2020-08-15 09:41:24 +0100

    dependencies with on items arent listed with links the links would fail to open as there are isn't a child stack to view DevSnicket/eunice#150

--- csharp/WriteInteractiveInDirectoryPathWithYaml/download-and-extract-package.sh
+++ csharp/WriteInteractiveInDirectoryPathWithYaml/download-and-extract-package.sh
@@ -3,5 +3,5 @@
 cd $(dirname "$0")
 
 rm devsnicket-eunice-interactive-*.tgz
-npm pack @devsnicket/eunice-interactive@5.9.2
+npm pack @devsnicket/eunice-interactive@5.9.3
 tar xvf devsnicket-eunice-interactive-*.tgz package/dist/index.html --strip-components 2
