5194ff3cb9e4629867ce4624f3857bd041f6fb82
Graham Dyson <grahamdyson@hotmail.com>
2020-08-20 07:55:02 +0100

    example tests fro parent inner and outer dependencies DevSnicket/eunice#156

--- visualizer/github-pages-copy.sh
+++ visualizer/github-pages-copy.sh
@@ -14,4 +14,7 @@ cp tests/test-cases/examples/stack/lower-depends-upon-upper/.svg gh-pages/exampl
 cp tests/test-cases/examples/stack/two-interdependent/.svg gh-pages/examples/stack/two-interdependent.svg &&
 cp tests/test-cases/examples/stack/upper-depends-upon-lower/.svg gh-pages/examples/stack/upper-depends-upon-lower.svg &&
 cp tests/test-cases/examples/two/.svg gh-pages/examples/two.svg &&
-cp tests/withParent/test-cases/examples/upper-item-depends-upon-lower-item-with-parent/.svg gh-pages/examples/upper-item-depends-upon-lower-item-with-parent.svg
+mkdir gh-pages/examples/parent &&
+cp tests/withParent/test-cases/examples/upper-child-depends-upon-lower-child/.svg gh-pages/examples/parent/upper-child-depends-upon-lower-child.svg
+cp tests/withParent/test-cases/examples/inner-dependencies-of-multiple-children/.svg gh-pages/examples/parent/inner-dependencies-of-multiple-children.svg
+cp tests/withParent/test-cases/examples/outer-dependencies/.svg gh-pages/examples/parent/outer-dependencies.svg
