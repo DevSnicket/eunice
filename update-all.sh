730870ae1697e9ae2f158839846837b842eb9c3d
Graham Dyson <grahamdyson@hotmail.com>
2020-09-09 09:30:47 +0100

    update all bash script

---
+++ update-all.sh
@@ -0,0 +1,4 @@
+node --experimental-modules updateMenusAndFooter/index.js
+(cd csharp/releases && node --experimental-modules updateReleasesListInHtmlFile.js --access-token=$GITHUB_HARNESSES_TOKEN)
+(cd issues && node --experimental-modules updateIssueListInHtmlFile.js --access-token=$GITHUB_HARNESSES_TOKEN)
+(cd javascript/releases && node --experimental-modules updateReleasesListInHtmlFile.js --access-token=$GITHUB_HARNESSES_TOKEN)
