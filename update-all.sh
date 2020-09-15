baf1d31b998317801fdfab57106a530a4069d419
Graham Dyson <grahamdyson@hotmail.com>
2020-09-15 07:03:00 +0100

    update all Bash script uses argument instead of global

--- update-all.sh
+++ update-all.sh
@@ -1,5 +1,5 @@
 node --experimental-modules updateMenusAndFooter/index.js
 node --experimental-modules licensing/updateFromSponsor.js
-(cd csharp/releases && node --experimental-modules updateReleasesListInHtmlFile.js --access-token=$GITHUB_HARNESSES_TOKEN)
-(cd issues && node --experimental-modules updateIssueListInHtmlFile.js --access-token=$GITHUB_HARNESSES_TOKEN)
-(cd javascript/releases && node --experimental-modules updateReleasesListInHtmlFile.js --access-token=$GITHUB_HARNESSES_TOKEN)
+(cd csharp/releases && node --experimental-modules updateReleasesListInHtmlFile.js --access-token=$1)
+(cd issues && node --experimental-modules updateIssueListInHtmlFile.js --access-token=$1)
+(cd javascript/releases && node --experimental-modules updateReleasesListInHtmlFile.js --access-token=$1)
