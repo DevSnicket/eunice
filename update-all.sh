171f3289a5de85c071c7fe9f2ba4c1bf9c610473
GitHub pages publish <grahamdyson@hotmail.com>
2022-04-18 17:22:06 +0100

    progress logging added to update all script

--- update-all.sh
+++ update-all.sh
@@ -1,5 +1,14 @@
+echo menus and footer
 node --experimental-modules updateMenusAndFooter/index.js
+
+echo csharp releases
 (cd csharp/releases && node --experimental-modules updateReleasesListInHtmlFile.js --access-token=$1)
+
+echo posts
 node --experimental-modules blog/updateFromPosts/index.js
+
+echo issues
 (cd issues && node --experimental-modules updateIssueListInHtmlFile.js --access-token=$1)
+
+echo javascript releases
 (cd javascript/releases && node --experimental-modules updateReleasesListInHtmlFile.js --access-token=$1)
