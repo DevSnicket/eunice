3638956f63f66820fd70e52a9f73fd46ddd707f8
Graham Dyson <grahamdyson@hotmail.com>
2024-12-28 12:13:31 +0000

    removed installation and test code coverage for .NET 3.1 and 5 now they are no longer supported #182

--- csharp/install.sh
+++ csharp/install.sh
@@ -6,8 +6,6 @@ curl -o dotnet-install.sh https://dotnet.microsoft.com/download/dotnet/scripts/v
 
 chmod +x dotnet-install.sh
 
-./dotnet-install.sh --channel 3.1
-./dotnet-install.sh --channel 5.0
 ./dotnet-install.sh --channel 6.0
 ./dotnet-install.sh --channel 7.0
 ./dotnet-install.sh --channel 8.0
--- csharp/test.sh
+++ csharp/test.sh
@@ -27,8 +27,6 @@ writeReport () {
 		echo report file not found $reportFile
 	fi
 }
-writeReport 3.1
-writeReport 5.0
 writeReport 6.0
 writeReport 7.0
 writeReport 8.0
